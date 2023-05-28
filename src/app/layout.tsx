9c0e94814f5286d8dda1771d02d5ecb91c8af166
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-05-28 23:05:50 +0200

    Refactor code formatting and remove unused commented line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,7 +13,7 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}): JSX.Element {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
