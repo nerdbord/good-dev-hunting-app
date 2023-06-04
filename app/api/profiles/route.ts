d5c9cf6d213d1bb936823ba177bf6eb08e376088
Jakub Wąsowski <jakub@nerdz.pl>
2023-06-04 22:09:07 +0200

    chore: remove redundant code

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -1,35 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import { getPublishedProfilesPayload } from '../../../backend/profile/profile.service'
-
-export async function allRequestController(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  httpHandlers: {
-    POST: (
-      req: NextApiRequest,
-      res: NextApiResponse,
-    ) => Promise<NextResponse<{ message: string }>>
-    GET: (
-      req: NextApiRequest,
-      res: NextApiResponse,
-    ) => Promise<NextResponse<{ message: string }>>
-  },
-) {
-  switch (req.method) {
-    case 'GET':
-      return httpHandlers.GET(req, res)
-    case 'POST':
-      return httpHandlers.POST(req, res)
-    default:
-      res.status(405).end(`Method ${req.method} Not Allowed`)
-  }
-}
-
-export const httpHandlers = {
-  GET,
-  POST,
-}
 
 export async function GET(request: NextApiRequest, response: NextApiResponse) {
   try {
