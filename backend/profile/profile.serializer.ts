9c70ec0130bcda7b76cf30bb0cd2b3c4606b09f5
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-06-06 19:59:47 +0200

    chore: cleanied unused code and typo

--- backend/profile/profile.serializer.ts
+++ backend/profile/profile.serializer.ts
@@ -1,42 +1,5 @@
 import { City, Country, Profile, User } from '@prisma/client'
 
-// export const serializeProfilesToProfilePayload = (
-//   data:
-//     | (Profile & {
-//         user: User
-//         country: Country
-//         city: City
-//       })[]
-//     | undefined,
-// ): ProfilePayload[] | undefined => {
-//   if (data) {
-//     return data.map((profile) => ({
-//       id: profile.id,
-//       fullName: profile.fullName,
-//       email: profile.user.email,
-//       linkedIn: profile.linkedIn,
-//       userId: profile.userId,
-//       bio: profile.bio,
-//       country: {
-//         name: profile.country.name,
-//         openForRelocation: profile.country.openForRelocation,
-//       },
-//       city: {
-//         name: profile.city.name,
-//         openForRelocation: profile.city.openForRelocation,
-//       },
-//       remoteOnly: profile.remoteOnly,
-//       position: profile.position,
-//       seniority: profile.seniority,
-//       techStack: profile.techStack,
-//       employmentType: profile.employmentType,
-//       isPublished: profile.isPublished,
-//     }))
-//   } else {
-//     return undefined
-//   }
-// }
-
 export const serializeProfilesToProfilePayload = (
   data: Profile & {
     user: User
