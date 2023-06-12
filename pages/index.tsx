5b939a48762d096f60d5c4c883599c8f38cc3401
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-06-12 20:27:21 +0200

    feat: add index page

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page from '../src/app/page'
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <Page />
+    </>
+  )
+}
+
+export default IndexPage
