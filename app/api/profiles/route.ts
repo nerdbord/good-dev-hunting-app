5f25a5d7b4619b0ac1c9965aebe76f7448708b62
Daniel Szczucki <dszczucki@gmail.com>
2023-06-12 07:14:32 +0200

    feat: added fn isUserExist and createUser, fadded return to serializer. added post profile route

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
-import { getPublishedProfilesPayload } from '../../../backend/profile/profile.service'
-import { prisma } from '../../../prisma/prismaClient'
 import {
-  CreateProfilePayload,
-  ProfileDataFromPrisma,
-  ProfilePayload,
-} from '../../../backend/profile/profile.types'
+  createUserProfile,
+  getPublishedProfilesPayload,
+  isUserProfileExist,
+} from '../../../backend/profile/profile.service'
+import { CreateProfilePayload } from '../../../backend/profile/profile.types'
 
 export async function GET(request: NextApiRequest, response: NextApiResponse) {
   try {
@@ -24,38 +23,18 @@ export async function GET(request: NextApiRequest, response: NextApiResponse) {
 
 export async function POST(request: Request) {
   try {
-    const body: CreateProfilePayload = await request.json()
-    console.log(body)
+    const userData: CreateProfilePayload = await request.json()
 
-    //call db
-    //check user by mail
-    const foundUser: ProfileDataFromPrisma = await prisma.profile.findFirst({
-      where: {
-        OR: [
-          { email: body.email },
-          //check full name?
-          { fullName: body.fullName },
-          //check linkenIn validation?
-          { linkedIn: body.linkedIn },
-        ],
-      },
-      include: {
-        user: true,
-        country: true,
-        city: true,
-      },
-    })
-    console.log(foundUser)
+    const foundUser = await isUserProfileExist(userData)
 
     if (!foundUser) {
       //create user
-      const newUser = 'user'
+      const newUser = await createUserProfile(userData)
+
       return new NextResponse(JSON.stringify(newUser), { status: 201 })
     } else {
       return new NextResponse('Such user arleady exist', { status: 409 })
     }
-
-    //return this user data
   } catch (error) {
     return new NextResponse('Something Went Wrong', { status: 500 })
   }
