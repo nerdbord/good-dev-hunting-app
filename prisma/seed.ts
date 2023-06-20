f65f8875aec7c518a5f33e719eca0053a7fd08a4
Jakub Wąsowski <jakub@nerdz.pl>
2023-06-20 15:54:02 +0200

    fix: prisma fixes

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, EmploymentType } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: 'dev@nerdbord.io' },
+    update: {},
+    create: {
+      email: 'dev@nerdbord.io',
+      profile: {
+        create: {
+          fullName: 'Richard Nerdbordowski',
+          remoteOnly: true,
+          bio: 'Little, sweet dog.',
+          position: 'Front-end developer',
+          seniority: 'Junior',
+          isPublished: true,
+          employmentType: EmploymentType.CONTRACT,
+          country: {
+            create: {
+              name: 'Poland',
+              openForRelocation: true,
+            },
+          },
+          city: {
+            create: {
+              name: 'Warsaw',
+              openForRelocation: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  console.log({ user })
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit()
+  })
