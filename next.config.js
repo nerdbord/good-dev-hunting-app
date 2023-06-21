a85bad0d6beaa5b762154a2cce5f66a511dacb26
AniaPiWo <snowflake@wp.pl>
2023-06-21 18:34:31 +0200

    feat: implementation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    ...nextConfig,
+    env: {
+        GIHUB_ID: process.env.GIHUB_ID,
+        GIHUB_SECRET: process.env.GIHUB_SECRET,
+    }
+}
