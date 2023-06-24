fd4e98352fdff593ba8c5ba6064aa40deef582fe
AniaPiWo <snowflake@wp.pl>
2023-06-24 17:05:34 +0200

    fix: usesession remove

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 import styles from './page.module.scss'
 import Filters from '@/components/Filters/Filters'
 
-
 const Home: React.FC = () => {
-
   return (
     <main className={styles.container}>
       <Filters />
