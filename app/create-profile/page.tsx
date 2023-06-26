f91ccb0eaf61bc7e11a63a6577ac2f4dc4afdaf3
AniaPiWo <snowflake@wp.pl>
2023-06-26 10:25:37 +0200

    fix: ssr part2

--- app/create-profile/page.tsx
+++ app/create-profile/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import React from 'react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { redirect } from 'next/navigation'
 
 const CreateProfilePage = async () => {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
   if (!!session) {
-  return (
-    <div>Create profile page</div>
-  )} else {
-    redirect("/");
+    return <div>Create profile page</div>
+  } 
+  if (!session) {
+    redirect('/')
   }
+}
 
-};
-
-export default CreateProfilePage;
+export default CreateProfilePage
