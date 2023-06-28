9715613cd4f1b6f89f2d7a399cf0bc29ba53a014
AniaPiWo <snowflake@wp.pl>
2023-06-28 20:36:54 +0200

    feat: user details

--- src/components/LogOutBtn/LogOutBtn.tsx
+++ src/components/LogOutBtn/LogOutBtn.tsx
@@ -1,11 +1,12 @@
 'use client'
 import React from 'react'
 import { signOut } from 'next-auth/react'
+import styles from "./LogOutBtn.module.scss"
 
 const LogOutBtn = () => {
   return (
     <div>
-      <button onClick={() => signOut()}>Log out</button>
+      <button className={styles.logout} onClick={() => signOut()}>Log out</button>
     </div>
   )
 }
