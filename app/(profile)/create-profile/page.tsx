cad1611ebb5d8e6b14d816098f4964126a0bcefa
AniaPiWo <snowflake@wp.pl>
2023-07-02 13:56:23 +0200

    feat: personal info ui

--- app/(profile)/create-profile/page.tsx
+++ app/(profile)/create-profile/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { redirect } from 'next/navigation'
-import LogOutBtn from '@/components/LogOutBtn/LogOutBtn'
+import styles from './page.module.scss'
+import PersonalInfo from '@/components/PersonalInfo/PersonalInfo'
 
 const CreateProfilePage = async () => {
   const session = await getServerSession(authOptions)
@@ -12,9 +13,14 @@ const CreateProfilePage = async () => {
   }
 
   return (
-    <div>
-      Create profile page
-      <br />
+    <div className={styles.main}>
+      <div  className={styles.titleBox}>
+        <span>Create profile page</span>
+        <button className={styles.saveProfileBtn}>Save and preview profile </button>
+      </div>
+      <div className={styles.formBox}>
+        <PersonalInfo/>
+      </div>
     </div>
   )
 }
