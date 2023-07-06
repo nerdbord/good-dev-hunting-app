92d9d72ecc08459fce1ea71495da4764ada1714b
AniaPiWo <snowflake@wp.pl>
2023-07-06 13:42:50 +0200

    feat: location form

---
+++ src/inputs/Checkbox/Checkbox.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react';
+import styles from './Checkbox.module.scss';
+import { IoIosCheckmark } from 'react-icons/io';
+
+interface CheckboxInputProps {
+  label: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const CheckboxInput: React.FC<CheckboxInputProps> = ({ label, checked, onChange }) => {
+
+  const handleCheckboxChange = () => {
+    onChange(!checked);
+  };
+
+  return (
+
+      <label className={styles.checkboxLabel}>
+        <div className={`${styles.checkbox} ${checked ? styles.checked : ''}`}>
+          <input
+            type="checkbox"
+            className={styles.hidden}
+            checked={checked}
+            onChange={handleCheckboxChange}
+          />
+          {checked && <IoIosCheckmark className={styles.checkmark} />} 
+        </div>
+      {label}
+    </label>
+
+  );
+};
+
+export default CheckboxInput;
