48a600ae6a605853c51d7d4b308afa8a82e03e9b
AniaPiWo <snowflake@wp.pl>
2023-07-07 18:16:46 +0200

    feat: work info create

---
+++ src/components/CreateProfile/WorkInformation/WorkInformation.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react'
+import TextArea from '@/inputs/TextArea/TextArea'
+import styles from './WorkInformations.module.scss'
+
+const WorkInformation = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div>Personal Information</div>
+        <div className={styles.personalInfo}>
+          Share personal information to let the recruiters get to know you.{' '}
+        </div>
+      </div>
+      <div className={styles.right}></div>
+    </div>
+  )
+}
+
+export default WorkInformation
