e86ba488cff3b7b671bfd578525cee4fe6864f07
AniaPiWo <snowflake@wp.pl>
2023-07-07 18:26:21 +0200

    feat: merge

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from 'react'
 import styles from './Container.module.scss'
 
-export const Container = (props: PropsWithChildren<{}>) => (
+type ContainerProps = PropsWithChildren<object>
+
+export const Container = (props: ContainerProps) => (
   <div className={styles.container}>{props.children}</div>
 )
--- src/components/CreateProfile/WorkInformation/WorkInformation.tsx
+++ src/components/CreateProfile/WorkInformation/WorkInformation.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import TextArea from '@/inputs/TextArea/TextArea'
 import styles from './WorkInformations.module.scss'
 
 const WorkInformation = () => {
