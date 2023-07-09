4f53cd347e2eaaa89bfa75a22c66e7e21251e648
Daniel Szczucki <dszczucki@gmail.com>
2023-07-09 20:51:40 +0200

    feat: added picture to schema, rebuild create user

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
+export async function GET() {
   const session = await getServerSession(authOptions)
   console.log(session?.user?.name)
 
