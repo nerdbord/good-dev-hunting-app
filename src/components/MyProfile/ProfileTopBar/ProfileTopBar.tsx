e8e567be2ac0815c83033a519daed824c96db992
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-08-07 15:23:38 +0200

    feat: added publish profile handler

--- src/components/MyProfile/ProfileTopBar/ProfileTopBar.tsx
+++ src/components/MyProfile/ProfileTopBar/ProfileTopBar.tsx
@@ -2,20 +2,45 @@
 import React from 'react'
 import styles from './ProfileTopBar.module.scss'
 import { Button } from '@/inputs/Button/Button'
+import { useSession } from 'next-auth/react'
 
 const ProfileTopBar = () => {
-  const handleButtonClick = () => {
-    console.log('clicked')
-  }
+
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+const handlePublishClick = async () => {
+    if (!userId) {
+      console.error('No user ID found')
+      return
+    }
+
+    const response = await fetch(`http://localhost:3000/api/profiles/${userId}/publish`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        isPublished: true,
+      }),
+    });
+
+    if (response.ok) {
+      console.log('Profile published successfully');
+    } else {
+      console.error('Failed to publish profile');
+    }
+  };
+
   return (
     <div className={styles.titleBox}>
       <span className={styles.title}>Profile preview</span>
       <div className={styles.buttonBox}>
-        <Button variant={'secondary'} onClick={handleButtonClick}>
+        <Button variant={'secondary'} onClick={() => console.log('click')}>
           {' '}
           Edit{' '}
         </Button>
-        <Button variant={'primary'} onClick={handleButtonClick}>
+        <Button variant={'primary'} onClick={handlePublishClick}>
           {' '}
           Publish profile{' '}
         </Button>
