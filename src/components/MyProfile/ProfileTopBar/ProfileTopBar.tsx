b03594be9d19d1b94d9cb7b8260ff1821e36fce4
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-08-07 15:34:46 +0200

    fix: removed clg and added handleEditClick fn to edit button

--- src/components/MyProfile/ProfileTopBar/ProfileTopBar.tsx
+++ src/components/MyProfile/ProfileTopBar/ProfileTopBar.tsx
@@ -3,40 +3,49 @@ import React from 'react'
 import styles from './ProfileTopBar.module.scss'
 import { Button } from '@/inputs/Button/Button'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { AppRoutes } from '@/utils/routes'
 
 const ProfileTopBar = () => {
+  const { data: session } = useSession()
+  const userId = session?.user?.id
 
-  const { data: session } = useSession();
-  const userId = session?.user?.id;
+  const router = useRouter()
 
-const handlePublishClick = async () => {
+  const handlePublishClick = async () => {
     if (!userId) {
       console.error('No user ID found')
       return
     }
 
-    const response = await fetch(`http://localhost:3000/api/profiles/${userId}/publish`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      `http://localhost:3000/api/profiles/${userId}/publish`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          isPublished: true,
+        }),
       },
-      body: JSON.stringify({
-        isPublished: true,
-      }),
-    });
-
+    )
     if (response.ok) {
-      console.log('Profile published successfully');
+      console.log('Profile published successfully')
     } else {
-      console.error('Failed to publish profile');
+      console.error('Failed to publish profile')
     }
-  };
+  }
+
+  const handleEditClick = () => {
+    router.push(AppRoutes.createProfile)
+  }
 
   return (
     <div className={styles.titleBox}>
       <span className={styles.title}>Profile preview</span>
       <div className={styles.buttonBox}>
-        <Button variant={'secondary'} onClick={() => console.log('click')}>
+        <Button variant={'secondary'} onClick={handleEditClick}>
           {' '}
           Edit{' '}
         </Button>
