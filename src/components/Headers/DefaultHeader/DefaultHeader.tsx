556f7a85c3b61810be052f28cdac3e49f0557d67
AniaPiWo <snowflake@wp.pl>
2023-08-16 09:35:46 +0200

    feat: publish

--- src/components/Headers/DefaultHeader/DefaultHeader.tsx
+++ src/components/Headers/DefaultHeader/DefaultHeader.tsx
@@ -4,14 +4,17 @@ import { Button } from '@/inputs/Button/Button'
 import styles from './DefaultHeader.module.scss'
 import logo from '@/assets/images/logo.png'
 import GithubIcon from '@/assets/icons/GithubIcon'
-import AddIcon from '@/assets/icons/AddIcon'
+/* import AddIcon from '@/assets/icons/AddIcon' */
 import Link from 'next/link'
 import { useSession, signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/dist/client/image'
 
 const DefaultHeader = () => {
-  const { status } = useSession()
-
+  const { status, data: session } = useSession()
+  const name = session?.user?.name
+  const avatar = session?.user?.image
+  const profileId = session?.user?.profileId
   const router = useRouter()
 
   if (status === 'authenticated') {
@@ -23,13 +26,38 @@ const DefaultHeader = () => {
         </Link>
         <div className={styles.actions}>
           <div className={styles.frameLogin}>
-            <Button
-              onClick={() => router.push('/my-profile')}
-              variant={'primary'}
-            >
-              {' '}
-              My profile{' '}
-            </Button>
+            <div>
+              <p className={styles.githubAccConnected}>
+                Connected Github account
+              </p>
+            </div>
+            <div className={styles.githubAcc}>
+              {avatar && (
+                <Image
+                  className={styles.githubAccImg}
+                  src={avatar}
+                  width={38}
+                  height={38}
+                  alt="github avatar"
+                />
+              )}
+              <p className={styles.githubAccName}>{name}</p>
+            </div>
+            {profileId === null ? (
+              <Button
+                onClick={() => router.push('/create-profile')}
+                variant={'primary'}
+              >
+                Create profile
+              </Button>
+            ) : (
+              <Button
+                onClick={() => router.push('/my-profile')}
+                variant={'primary'}
+              >
+                My profile
+              </Button>
+            )}
           </div>
         </div>
       </header>
@@ -47,13 +75,6 @@ const DefaultHeader = () => {
         <Button onClick={() => signIn('github')} variant={'secondary'}>
           Login
           <GithubIcon />
-        </Button>
-        <Button
-          onClick={() => router.push('/create-profile')}
-          variant={'primary'}
-        >
-          Create profile
-          <AddIcon />
         </Button>
       </div>
     </header>
