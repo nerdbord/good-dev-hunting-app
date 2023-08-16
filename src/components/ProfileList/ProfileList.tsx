9ee1c6afad753b5611c9babf99d6be33116d3c45
hansac91 <saczukhanna@gmail.com>
2023-08-16 16:53:40 +0200

    feat: fetching profiles from server

--- src/components/ProfileList/ProfileList.tsx
+++ src/components/ProfileList/ProfileList.tsx
@@ -1,30 +1,21 @@
-'use client'
 import React from 'react'
 import styles from '@/components/ProfileList/ProfileList.module.scss'
 import ProfilePicture from '@/assets/images/ProfilePicture.png'
-import {
-  profileData,
-  ProfileListItems,
-  JobSpecialization,
-} from '@/components/ProfileList/profile-data'
-import { useFilters } from '@/contexts/FilterContext'
-import {
-  filterByJobSpecialization,
-  filterBySeniority,
-  filterByLocation,
-  filterByTechnology,
-  filterByAvailability,
-} from './filters'
+import { JobSpecialization } from '@/components/ProfileList/profile-data'
 import classNames from 'classnames/bind'
+import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
+import { ProfileWithRelations } from '@/backend/profile/profile.types'
+import ProfilesWithFilter from '@/components/ProfileList/ProfilesWithFilter'
+
 const cx = classNames.bind(styles)
 
-export const ProfileListItem: React.FC<{ data: ProfileListItems }> = ({
+export const ProfileListItem: React.FC<{ data: ProfileWithRelations }> = ({
   data,
 }) => {
   const commonClasses = {
-    [styles.frontend]: data.jobSpecialization === JobSpecialization.Frontend,
-    [styles.backend]: data.jobSpecialization === JobSpecialization.Backend,
-    [styles.fullstack]: data.jobSpecialization === JobSpecialization.Fullstack,
+    [styles.frontend]: data.position === JobSpecialization.Frontend,
+    [styles.backend]: data.position === JobSpecialization.Backend,
+    [styles.fullstack]: data.position === JobSpecialization.Fullstack,
   }
 
   const getStackClasses = cx(commonClasses)
@@ -34,13 +25,13 @@ export const ProfileListItem: React.FC<{ data: ProfileListItems }> = ({
   })
 
   const renderTechnologies = () => {
-    if (data.technology.length <= 4) {
-      return data.technology.map((tech, index) => (
+    if (data.techStack.length <= 4) {
+      return data.techStack.map((tech, index) => (
         <span key={index}>{tech}</span>
       ))
     } else {
-      const displayedTechnologies = data.technology.slice(0, 3)
-      const othersCount = data.technology.length - 3
+      const displayedTechnologies = data.techStack.slice(0, 3)
+      const othersCount = data.techStack.length - 3
 
       return (
         <>
@@ -60,12 +51,12 @@ export const ProfileListItem: React.FC<{ data: ProfileListItems }> = ({
           <img src={ProfilePicture.src} alt="Profile Picture" />
         </div>
         <div className={styles.data}>
-          <p className={styles.name}>{data.name}</p>
+          <p className={styles.name}>{data.fullName}</p>
           <p className={getStackClasses}>
-            {data.seniority} {data.jobSpecialization} Developer
+            {data.seniority} {data.position} Developer
           </p>
           <p className={styles.location}>
-            {data.country}, {data.city} / {data.remote}
+            {data.country.name}, {data.city.name} / {''}
           </p>
         </div>
       </div>
@@ -74,34 +65,22 @@ export const ProfileListItem: React.FC<{ data: ProfileListItems }> = ({
   )
 }
 
-const ProfileList: React.FC = () => {
-  const {
-    jobSpecializationFilter,
-    technologyFilter,
-    seniorityFilter,
-    availabilityFilter,
-    locationFilter,
-  } = useFilters()
+const ProfileList = async () => {
+  const profiles = await getPublishedProfilesPayload()
+  console.log('profile', profiles)
 
-  const filteredProfileData = profileData
-    .filter(filterByJobSpecialization(jobSpecializationFilter))
-    .filter(filterBySeniority(seniorityFilter))
-    .filter(filterByLocation(locationFilter))
-    .filter(filterByTechnology(technologyFilter))
-    .filter(filterByAvailability(availabilityFilter))
-
-  const filteredCount = filteredProfileData.length
+  const filteredCount = profiles.length
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>Profiles found ({filteredCount})</div>
       <div className={styles.profileListCont}>
-        {filteredProfileData.map((profile) => (
+        {profiles.map((profile) => (
           <ProfileListItem key={profile.id} data={profile} />
         ))}
+        <ProfilesWithFilter data={profile} />
       </div>
     </div>
   )
 }
-
 export default ProfileList
