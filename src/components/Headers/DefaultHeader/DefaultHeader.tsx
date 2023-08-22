37aa2447aa5dab6024864983ece2c3ef86ee5f69
AniaPiWo <snowflake@wp.pl>
2023-08-22 18:53:11 +0200

    feat: headers

--- src/components/Headers/DefaultHeader/DefaultHeader.tsx
+++ src/components/Headers/DefaultHeader/DefaultHeader.tsx
@@ -7,14 +7,64 @@ import GithubIcon from '@/assets/icons/GithubIcon'
 import Link from 'next/link'
 import { useSession, signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/dist/client/image'
 import { AppRoutes } from '@/utils/routes'
+import AddIcon from '@/assets/icons/AddIcon'
 
 const DefaultHeader = () => {
   const { status, data: session } = useSession()
-  /*   const name = session?.user?.name
+  const name = session?.user?.name
   const avatar = session?.user?.image
-  const profileId = session?.user?.profileId */
+  const profileId = session?.user?.profileId
   const router = useRouter()
+  console.log(profileId)
+
+  if (status === 'authenticated') {
+    return (
+      <header className={styles.wrapper}>
+        <Link href="/" className={styles.logo}>
+          <img src={logo.src} alt="Logo" />
+          <div className={styles.title}>Good Dev Hunting</div>
+        </Link>
+        <div className={styles.actions}>
+          <div className={styles.frameLogin}>
+            <p className={styles.githubAccConnected}>
+              Connected Github account
+            </p>
+
+            <div className={styles.githubAcc}>
+              {avatar && (
+                <Image
+                  className={styles.githubAccImg}
+                  src={avatar}
+                  width={38}
+                  height={38}
+                  alt="github avatar"
+                />
+              )}
+              <p className={styles.githubAccName}>{name}</p>
+            </div>
+            {profileId === null ? (
+              <Button
+                onClick={() => router.push(AppRoutes.createProfile)}
+                variant={'primary'}
+              >
+                Create profile
+                <AddIcon />
+              </Button>
+            ) : (
+              <Button
+                onClick={() => router.push(AppRoutes.myProfile)}
+                variant={'primary'}
+              >
+                My profile
+              </Button>
+            )}
+          </div>
+        </div>
+      </header>
+    )
+  }
 
   return (
     <header className={styles.wrapper}>
@@ -22,29 +72,14 @@ const DefaultHeader = () => {
         <img src={logo.src} alt="Logo" />
         <div className={styles.title}>Good Dev Hunting</div>
       </Link>
-      {status === 'authenticated' ? (
-        <div className={styles.frameButtons}>
-          <Button
-            onClick={() => router.push(AppRoutes.myProfile)}
-            variant={'primary'}
-          >
-            My profile
-          </Button>
-        </div>
-      ) : (
-        <div className={styles.frameButtons}>
-          <Button onClick={() => signIn('github')} variant={'secondary'}>
-            Login
-            <GithubIcon />
-          </Button>
-          <Button onClick={() => signIn()} variant={'primary'}>
-            Create profile
-            {/*             <AddIcon /> */}
-          </Button>
-        </div>
-      )}
+
+      <div className={styles.frameButtons}>
+        <Button onClick={() => signIn('github')} variant={'secondary'}>
+          Login
+          <GithubIcon />
+        </Button>
+      </div>
     </header>
   )
 }
-
 export default DefaultHeader
