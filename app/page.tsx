960fa298976acc244222abb7d81a4a519d68defc
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-09-11 13:01:36 +0200

    fix: ssr headers

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import Filters from '@/components/Filters/Filters'
 import DefaultHeader from '@/components/Headers/DefaultHeader/DefaultHeader'
 import ProfileList from '@/components/ProfileList/ProfileList'
 import { FiltersProvider } from '@/contexts/FilterContext'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
-const Home: React.FC = () => {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
   return (
     <FiltersProvider>
-      <DefaultHeader />
+      <DefaultHeader session={session} />
       <Filters />
       {/* @ts-expect-error Server Component */}
       <ProfileList />
     </FiltersProvider>
   )
 }
-
-export default Home
