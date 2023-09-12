5e207d70d0393907bdf0f7deb61cecf38d001003
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-09-12 10:07:54 +0200

    fix: publish in MyProfile topbar

---
+++ src/components/PublishProfileBtn/PublishProfileBtn.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import { Button } from '@/components/Button/Button'
+import { apiClient } from '@/lib/apiClient'
+import { useSession } from 'next-auth/react'
+
+const PublishProfileBtn = () => {
+  const { data: session } = useSession()
+  const id = session?.user.id
+  const profile = session?.user.profile
+
+  const handlePublishClick = async () => {
+    console.log(id)
+    console.log(profile)
+
+    if (!id) {
+      throw new Error(
+        'Missing profile ID, please check if the authorization was successful.',
+      )
+    }
+
+    if (id && profile) {
+      try {
+        await apiClient.publishMyProfile(id)
+        console.log('Profile published successfully')
+      } catch (error) {
+        console.error('Failed to publish profile', error)
+      }
+    }
+  }
+
+  return (
+    <Button variant={'primary'} onClick={handlePublishClick}>
+      {' '}
+      Publish profile{' '}
+    </Button>
+  )
+}
+
+export default PublishProfileBtn
