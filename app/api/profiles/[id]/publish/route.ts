aa927a0919c43aa94e17b17b0639c16d145dae71
Jakub Wąsowski <jakub@nerdz.pl>
2023-10-02 18:51:58 +0200

    fix: fixing headers

--- app/api/profiles/[id]/publish/route.ts
+++ app/api/profiles/[id]/publish/route.ts
@@ -1,30 +1,38 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getProfileById, updateUserData } from '@/backend/profile/profile.service'
+import {
+  getProfileById,
+  updateUserData,
+} from '@/backend/profile/profile.service'
 import { authorizeUser } from '@/lib/auth'
 
-export async function POST(request: NextRequest, idObj: { params: { id: string } }) {
-  const id = idObj.params.id;
+export async function POST(
+  request: NextRequest,
+  idObj: { params: { id: string } },
+) {
+  const id = idObj.params.id
   try {
     await authorizeUser()
 
     const existingProfile = await getProfileById(id)
-    
+
     if (!existingProfile) {
       return new NextResponse('Profile not found', { status: 404 })
     }
-    
+
     const updateUserProfileById = await updateUserData(id, {
-      isPublished: !existingProfile.isPublished
+      isPublished: !existingProfile.isPublished,
     })
 
-    const serializedProfile = JSON.stringify(updateUserProfileById);
+    const serializedProfile = JSON.stringify(updateUserProfileById)
 
-    return new NextResponse(serializedProfile, { 
+    return new NextResponse(serializedProfile, {
       status: 200,
-      headers: { 'Content-Type': 'application/json' } 
+      headers: { 'Content-Type': 'application/json' },
     })
   } catch (error) {
     console.log('Error:', error)
-    return new NextResponse(`Error: ${(error as Error).message}`, { status: 500 })
+    return new NextResponse(`Error: ${(error as Error).message}`, {
+      status: 500,
+    })
   }
 }
