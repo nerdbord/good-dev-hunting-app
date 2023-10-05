c50767f1c2ba1436c1264e8eefaf117f35202df4
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-10-05 20:41:12 +0200

    fix: testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Filters from '@/components/Filters/Filters'
 import DefaultHeader from '@/components/Headers/DefaultHeader/DefaultHeader'
 import ProfileList from '@/components/ProfileList/ProfileList'
 import { FiltersProvider } from '@/contexts/FilterContext'
-
+// nerdy
 export default function Home() {
   return (
     <FiltersProvider>
