439a5de25cf7736ead1ed447dd1195eb737ca3a5
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-20 21:16:07 +0200

    fix: add redirecting to createProfile

--- src/components/CreateProfileBtn/CreateProfileBtn.tsx
+++ src/components/CreateProfileBtn/CreateProfileBtn.tsx
@@ -4,9 +4,16 @@ import { Button } from '@/components/Button/Button'
 import AddIcon from '@/assets/icons/AddIcon'
 import { signIn, useSession } from 'next-auth/react'
 import { AppRoutes } from '@/utils/routes'
+import { usePathname, useRouter } from 'next/navigation'
 
 const CreateProfileBtn = () => {
+  const router = useRouter()
+  const path = usePathname()
   const { data: session } = useSession()
+
+  if (path.includes(AppRoutes.myProfile)) {
+    return null
+  }
 
   const onClickHandler = async () => {
     if (!session) {
@@ -15,7 +22,9 @@ const CreateProfileBtn = () => {
       })
       return
     }
+    router.push(AppRoutes.createProfile)
   }
+
   return (
     <Button onClick={onClickHandler} variant={'primary'}>
       Create profile
