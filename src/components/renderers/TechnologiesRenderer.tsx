a8dc57e01c476f94e5e7c699d5f69236c335576c
darionauta <dariusz.stanek@gmail.com>
2023-10-23 21:37:51 +0200

    fix: displaying profiles based on selected tab

--- src/components/renderers/TechnologiesRenderer.tsx
+++ src/components/renderers/TechnologiesRenderer.tsx
@@ -1,32 +1,31 @@
 import { ProfileModel } from '@/data/frontend/profile/types'
 
 type Props = {
-    data: ProfileModel,
-    classes: string
+  data: ProfileModel
+  classes: string
 }
 
-export default function TechnologiesRenderer({data, classes}: Props){
+export default function TechnologiesRenderer({ data, classes }: Props) {
+  const renderTechnologies = () => {
+    const sliceCount = window.innerWidth <= 768 ? 2 : 3
 
-    const renderTechnologies = () => {
-        const sliceCount = window.innerWidth <= 768 ? 2 : 3
-    
-        if (data.techStack.length <= sliceCount) {
-          return data.techStack.map((tech, index) => (
+    if (data.techStack.length <= sliceCount) {
+      return data.techStack.map((tech, index) => (
+        <span key={index}>{tech}</span>
+      ))
+    } else {
+      const displayedTechnologies = data.techStack.slice(0, sliceCount)
+      const othersCount = data.techStack.length - sliceCount
+      return (
+        <>
+          {displayedTechnologies.map((tech, index) => (
             <span key={index}>{tech}</span>
-          ))
-        } else {
-          const displayedTechnologies = data.techStack.slice(0, sliceCount)
-          const othersCount = data.techStack.length - sliceCount
-          return (
-            <>
-              {displayedTechnologies.map((tech, index) => (
-                <span key={index}>{tech}</span>
-              ))}
-              <span>{`+ ${othersCount} Others`}</span>
-            </>
-          )
-        }
+          ))}
+          <span>{`+ ${othersCount} Others`}</span>
+        </>
+      )
     }
+  }
 
-    return <div className={classes}>{renderTechnologies()}</div>
+  return <div className={classes}>{renderTechnologies()}</div>
 }
