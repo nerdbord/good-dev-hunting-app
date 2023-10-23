fd6991db01b847b1d7b28dfb6d09ef1f14648757
darionauta <dariusz.stanek@gmail.com>
2023-10-23 21:37:20 +0200

    fix: displaying profiles based on selected tab

--- src/components/FilterTabs/Tabs.tsx
+++ src/components/FilterTabs/Tabs.tsx
@@ -3,15 +3,22 @@ import { useState } from 'react'
 import Tab from './Tab'
 import styles from './tabs.module.scss'
 import { PublishingState } from '@prisma/client'
+import { useModerationFilter } from '@/contexts/ModerationFilterContext'
 
 export const availableTabs: PublishingState[] = Object.values(PublishingState)
   .filter((key) => key !== 'DRAFT')
   .map((key) => PublishingState[key] as PublishingState)
 
 export default function Tabs() {
+  const { setPublishingStateFilter } = useModerationFilter()
   const [activeTab, setActiveTab] = useState<PublishingState>(
     PublishingState.PENDING,
   )
+
+  const setModerationFilter = (tab: PublishingState) => {
+    setActiveTab(PublishingState[tab])
+    setPublishingStateFilter && setPublishingStateFilter(tab)
+  }
 
   return (
     <div className={styles.tabsWrapper}>
@@ -21,7 +28,7 @@ export default function Tabs() {
             key={index}
             active={tab === activeTab}
             name={tab}
-            action={() => setActiveTab(PublishingState[tab])}
+            action={() => setModerationFilter(PublishingState[tab])}
             counter={0}
           />
         )
