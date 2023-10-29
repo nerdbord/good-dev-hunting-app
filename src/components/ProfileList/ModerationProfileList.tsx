f8c8a0b9f0efeea07d118641aacb4e34a2739c6f
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-29 13:40:45 +0100

    feat: display searching info when clicking on search button

--- src/components/ProfileList/ModerationProfileList.tsx
+++ src/components/ProfileList/ModerationProfileList.tsx
@@ -1,16 +1,14 @@
-import styles from '@/components/ProfileList/ProfileList.module.scss'
-import { Container } from '@/components/Container/Container'
 import ModerationProfilesWithFilter from '@/components/ProfileList/ModerationProfilesWithFilter'
 import { getAllPublishedProfilesPayload } from '@/backend/profile/profile.service'
+
+import styles from './ProfileList.module.scss'
 
 export default async function ModerationProfileList() {
   const profiles = await getAllPublishedProfilesPayload()
 
   return (
     <div className={styles.mainContainer}>
-      <Container>
-        <ModerationProfilesWithFilter data={profiles} />
-      </Container>
+      <ModerationProfilesWithFilter data={profiles} />
     </div>
   )
 }
--- src/components/ProfileList/ModerationProfilesWithFilter.tsx
+++ src/components/ProfileList/ModerationProfilesWithFilter.tsx
@@ -1,32 +1,68 @@
 'use client'
 import React from 'react'
-import styles from './ProfileList.module.scss'
-import { ProfileModel } from '@/data/frontend/profile/types'
+import { Button } from '../Button/Button'
 import { ModerationProfileListItem } from './ModerationProfileListItem'
+import { ProfileModel } from '@/data/frontend/profile/types'
 import { useModerationFilter } from '@/contexts/ModerationFilterContext'
 import useTabCounter from '@/hooks/useTabCounter'
 import { PublishingState } from '@prisma/client'
+
+import styles from './ProfileList.module.scss'
 
 type Props = {
   data: ProfileModel[]
 }
 
-export default function ({ data = [] }: Props) {
-  const { publishingStateFilter, setPendingStateCounter } =
+export default function ModerationProfilesWithFilters({ data = [] }: Props) {
+  const { publishingStateFilter, setPendingStateCounter, searchEmailValue } =
     useModerationFilter()
 
-  const filteredData = data.filter((profile: ProfileModel) => {
-    return profile.state === publishingStateFilter
+  const filteredData = data.filter((user: ProfileModel) => {
+    return user?.state === publishingStateFilter
   })
 
   setPendingStateCounter &&
     useTabCounter(data, PublishingState.PENDING, setPendingStateCounter)
 
+  if (searchEmailValue !== '') {
+    return (
+      <div className={styles.moderationProfiles}>
+        <div className={styles.searchInfoCont}>
+          <p className={styles.searchInfo}>
+            Search results for{' '}
+            <span className={styles.searchValue}>"{searchEmailValue}" (0)</span>
+          </p>
+          <Button variant="action">Clear search</Button>
+        </div>
+        <div className={styles.searchEmptyInfoCont}>
+          <p className={styles.searchInfo}>
+            No search results for{' '}
+            <span className={styles.searchValue}>"{searchEmailValue}" (0)</span>
+          </p>
+          <Button variant="primary">Clear search</Button>
+        </div>
+        <div className={styles.profileListCont}>
+          {filteredData.map((profile) => {
+            if (profile) {
+              return (
+                <ModerationProfileListItem key={profile.id} data={profile} />
+              )
+            }
+          })}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className={styles.profileListCont}>
-      {filteredData.map((profile) => (
-        <ModerationProfileListItem key={profile.id} data={profile} />
-      ))}
+    <div className={styles.moderationProfiles}>
+      <div className={styles.profileListCont}>
+        {filteredData.map((profile) => {
+          if (profile) {
+            return <ModerationProfileListItem key={profile.id} data={profile} />
+          }
+        })}
+      </div>
     </div>
   )
 }
