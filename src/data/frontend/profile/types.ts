e936b5e3c99bbd241220bb65f17dc2d84d2986b8
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-29 13:08:30 +0100

    feat: display suggestions when typing in search input

--- src/data/frontend/profile/types.ts
+++ src/data/frontend/profile/types.ts
@@ -22,7 +22,8 @@ export interface ProfileModel {
   employmentType: EmploymentType
   githubUsername: string | null
   state: PublishingState
+  userEmail: string
 }
 
-export type CreateProfilePayload = Omit<ProfileModel, 'id'>
+export type CreateProfilePayload = Omit<ProfileModel, 'id' | 'userEmail'>
 export type EditProfilePayload = CreateProfilePayload
