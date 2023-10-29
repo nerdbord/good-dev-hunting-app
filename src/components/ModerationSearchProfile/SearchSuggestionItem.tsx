3101195f64efcb67ed840aa0e55ad764ceb9d6cb
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-29 13:08:30 +0100

    feat: display suggestions when typing in search input

---
+++ src/components/ModerationSearchProfile/SearchSuggestionItem.tsx
@@ -0,0 +1,49 @@
+﻿import { ProfileModel } from '@/data/frontend/profile/types'
+
+import styles from './SearchSuggestionItem.module.scss'
+
+interface SearchSuggestionsProps {
+  searchValue: string
+  onClick: (text: string) => void
+  user: ProfileModel
+}
+
+const highlightText = (text: string, searchText: string) => {
+  const parts = text.split(new RegExp(`(${searchText})`, 'gi'))
+  return (
+    <p className={styles.userEmail}>
+      {parts.map((part, index) =>
+        part.toLowerCase() === searchText.toLowerCase() ? (
+          <span key={index} className={styles.highlighted}>
+            {part}
+          </span>
+        ) : (
+          part
+        ),
+      )}
+    </p>
+  )
+}
+
+export const SearchSuggestionItem = ({
+  searchValue,
+  onClick,
+  user,
+}: SearchSuggestionsProps) => {
+  return (
+    <li
+      className={styles.suggestionItem}
+      onClick={() => onClick(user.userEmail)}
+    >
+      <img
+        src={user.avatarUrl ? user.avatarUrl : ''}
+        alt="img"
+        className={styles.suggestionImg}
+      />
+      <div>
+        <p className={styles.userName}>{user.fullName}</p>
+        {highlightText(user.userEmail, searchValue)}
+      </div>
+    </li>
+  )
+}
--- src/components/ModerationSearchProfile/SearchWrapper.tsx
+++ src/components/ModerationSearchProfile/SearchWrapper.tsx
@@ -1,15 +1,48 @@
-import styles from './search.module.scss'
+'use client'
+import { ChangeEvent, useState } from 'react'
 import { Button } from '../Button/Button'
+import { SearchSuggestionItem } from './SearchSuggestionItem'
+import { ProfileModel } from '@/data/frontend/profile/types'
 
-export default function SearchWrapper() {
+import styles from './SearchWrapper.module.scss'
+
+type Props = {
+  data: ProfileModel[]
+}
+
+export default function SearchWrapper({ data = [] }: Props) {
+  const [searchValue, setSearchValue] = useState('')
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value)
+  }
+
+  const filteredUsers = data.filter((user) => {
+    return searchValue !== '' && user.userEmail.includes(searchValue)
+  })
+
   return (
     <div className={styles.searchWrapper}>
       <input
         name="searchValue"
         className={styles.searchInput}
         placeholder="eg. richard@gmail.com"
+        onChange={changeHandler}
+        value={searchValue}
       />
       <Button variant={'action'}>Search</Button>
+      {filteredUsers.length > 0 && (
+        <ul className={styles.suggestionsBox}>
+          {filteredUsers.map((user) => (
+            <SearchSuggestionItem
+              key={user.id}
+              searchValue={searchValue}
+              onClick={setSearchValue}
+              user={user}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
