edc130f6f2d4e0555a99c7955496e364b0a5edda
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-29 14:14:23 +0100

    feat: unchecked tabs when click on search button

--- src/components/FilterTabs/Tabs.tsx
+++ src/components/FilterTabs/Tabs.tsx
@@ -1,24 +1,27 @@
 'use client'
-import { useState } from 'react'
 import Tab from './Tab'
-import styles from './tabs.module.scss'
 import { PublishingState } from '@prisma/client'
 import { useModerationFilter } from '@/contexts/ModerationFilterContext'
+
+import styles from './tabs.module.scss'
 
 export const availableTabs: PublishingState[] = Object.values(PublishingState)
   .filter((key) => key !== 'DRAFT')
   .map((key) => PublishingState[key] as PublishingState)
 
 export default function Tabs() {
-  const { pendingStateCounter, setPublishingStateFilter } =
-    useModerationFilter()
-  const [activeTab, setActiveTab] = useState<PublishingState>(
-    PublishingState.PENDING,
-  )
+  const {
+    pendingStateCounter,
+    setPublishingStateFilter,
+    activeTab,
+    setActiveTab,
+    setEmailSearchValue,
+  } = useModerationFilter()
 
   const setModerationFilter = (tab: PublishingState) => {
-    setActiveTab(PublishingState[tab])
+    setActiveTab && setActiveTab(PublishingState[tab])
     setPublishingStateFilter && setPublishingStateFilter(tab)
+    setEmailSearchValue && setEmailSearchValue('')
   }
 
   const nameWithCounter = (counter: number, name: string) =>
