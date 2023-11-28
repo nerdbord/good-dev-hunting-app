10611f53775484c956bc2b220127a4877f18a5c0
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-28 17:42:11 +0100

    chore: use useModal for each modal component

--- src/components/ContactForm/ContactBtn/ContactBtn.tsx
+++ src/components/ContactForm/ContactBtn/ContactBtn.tsx
@@ -1,42 +1,37 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from '@/components/Button/Button'
-
 import ContactFormModal from '../ContactFormModal/ContactFormModal'
 import { ProfileModel } from '@/data/frontend/profile/types'
 import { useModal } from '@/contexts/ModalContext'
 import ContactSuccessModal from '../ContactSuccessModal/ContactSuccessModal'
 
 const ContactBtn = ({ userProfile }: { userProfile: ProfileModel }) => {
-  const [showFormModal, setShowFormModal] = useState<boolean>(false)
   const { showModal, closeModal } = useModal()
 
   return (
     <div>
       <Button
         onClick={() => {
-          setShowFormModal(true)
+          showModal(
+            <ContactFormModal
+              userProfile={userProfile}
+              showSuccessMsg={() => {
+                showModal(
+                  <ContactSuccessModal
+                    userProfile={userProfile}
+                    onClose={closeModal}
+                  />,
+                )
+              }}
+              closeModal={closeModal}
+            />,
+          )
         }}
         variant={'primary'}
       >
         Contact
       </Button>
-
-      {showFormModal && (
-        <ContactFormModal
-          userProfile={userProfile}
-          showSuccessMsg={() => {
-            setShowFormModal(false)
-            showModal(
-              <ContactSuccessModal
-                userProfile={userProfile}
-                onClose={closeModal}
-              />,
-            )
-          }}
-          closeModal={() => setShowFormModal(false)}
-        />
-      )}
     </div>
   )
 }
