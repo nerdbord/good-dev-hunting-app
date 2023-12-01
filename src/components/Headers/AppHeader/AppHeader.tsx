c5bb9d2dde5c9bba540bfe546ef17fb93148ab85
AniaPiWo <snowflake@wp.pl>
2023-12-01 20:39:25 +0100

    fix: last fixes

--- src/components/Headers/AppHeader/AppHeader.tsx
+++ src/components/Headers/AppHeader/AppHeader.tsx
@@ -19,9 +19,6 @@ const AppHeader = async () => {
     ? await getProfileByUserEmail(session.user.email)
     : null
 
-  console.log(session)
-  console.log(profile)
-
   if (session) {
     return (
       <header className={styles.wrapper}>
