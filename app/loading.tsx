fc5ff2bcd561392a5e92ca185005e55b11aaf394
AniaPiWo <snowflake@wp.pl>
2023-12-01 14:29:54 +0100

    fix: switch, loader

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './page.module.scss'
+const Loading = () => {
+  return <p className={styles.loading}>Fetching that data for you...</p>
+}
+
+export default Loading
