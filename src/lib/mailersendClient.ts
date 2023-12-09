bc747724436f3577bdf0b5d03cacc633aebbcc3e
agnbart <polpumi@gmail.com>
2023-12-09 14:44:46 +0100

    feat: welcome email after registration from github

--- src/lib/mailersendClient.ts
+++ src/lib/mailersendClient.ts
@@ -2,7 +2,7 @@ import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend'
 import { APIResponse } from 'mailersend/lib/services/request.service'
 
 export const mailersendClient = {
-  async sendMail(email: string, name?: string) {
+  async sendMail(email: string, name: string) {
     const mailerSend = new MailerSend({
       apiKey: process.env.MAILERSEND_API_KEY!,
     })
@@ -14,15 +14,50 @@ export const mailersendClient = {
 
     const recipients = [new Recipient(email, name)]
 
+    interface Substitution {
+      var: string
+      value: string
+    }
+
+    interface Variable {
+      email: string
+      substitutions: Substitution[]
+    }
+
+    const variables: Variable[] = [
+      {
+        email: email,
+        substitutions: [
+          {
+            var: 'name',
+            value: name,
+          },
+          {
+            var: 'account.name',
+            value: process.env.MAILERSEND_FROM_NAME!,
+          },
+          {
+            var: 'support_email',
+            value: process.env.MAILERSEND_FROM!,
+          },
+        ],
+      },
+    ]
+
     const emailParams = new EmailParams()
       .setFrom(sentFrom)
       .setTo(recipients)
       .setReplyTo(sentFrom)
       .setSubject('Welcome to Good Dev Hunting')
       .setTemplateId(process.env.MAILERSEND_TEMPLATE_ID!)
+      .setVariables(variables)
 
     const response: APIResponse = await mailerSend.email.send(emailParams)
-    if (response.statusCode !== 200) {
+    if (response.statusCode < 300) {
+      console.log(
+        ` Email was sent to ${email}. Response status code = ${response.statusCode}`,
+      )
+    } else {
       console.error(
         `Problem with sending email to ${email}. Response status code = ${response.statusCode}`,
       )
