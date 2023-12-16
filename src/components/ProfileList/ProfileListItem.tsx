51ffc29f14c95cc70813fbc542eea3f561a01df2
The Fox <lisieckikamil93@gmail.com>
2023-12-16 21:59:22 +0100

    fix(profile-card): update styles and avatar data

--- src/components/ProfileList/ProfileListItem.tsx
+++ src/components/ProfileList/ProfileListItem.tsx
@@ -8,6 +8,7 @@ import { ProfileModel } from '@/data/frontend/profile/types'
 import { AppRoutes } from '@/utils/routes'
 import { useRouter } from 'next/navigation'
 import TechnologiesRenderer from '@/components/renderers/TechnologiesRenderer'
+import Image from 'next/image'
 
 const cx = classNames.bind(styles)
 
@@ -27,26 +28,34 @@ export const ProfileListItem: React.FC<{ data: ProfileModel }> = ({ data }) => {
   })
 
   return (
-    <div
-      className={styles.frame}
-      onClick={() => router.push(`${AppRoutes.userProfile}/${data.userId}`)}
-    >
-      <div className={styles.container} data-test-id="profileContainer">
-        <div className={styles.profile}>
-          <img src={ProfilePicture.src} alt="Profile Picture" />
+    <>
+      <div
+        className={styles.frame}
+        onClick={() => router.push(`${AppRoutes.userProfile}/${data.userId}`)}
+      >
+        <div className={styles.container} data-test-id="profileContainer">
+          <div className={styles.profile}>
+            <Image
+              src={data.avatarUrl || ''}
+              width={78}
+              height={78}
+              alt="user's avatar"
+              className={styles.avatar}
+            />{' '}
+          </div>
+          <div className={styles.data}>
+            <p className={styles.name}>{data.fullName}</p>
+            <p className={getStackClasses}>
+              {data.seniority} {data.position} Developer
+            </p>
+            <p className={styles.location}>
+              {data.country.name}, {data.city.name} /{' '}
+              {data.remoteOnly && 'Remote'}
+            </p>
+          </div>
         </div>
-        <div className={styles.data}>
-          <p className={styles.name}>{data.fullName}</p>
-          <p className={getStackClasses}>
-            {data.seniority} {data.position} Developer
-          </p>
-          <p className={styles.location}>
-            {data.country.name}, {data.city.name} /{' '}
-            {data.remoteOnly && 'Remote'}
-          </p>
-        </div>
+        <TechnologiesRenderer data={data} classes={getTechnologyClasses} />
       </div>
-      <TechnologiesRenderer data={data} classes={getTechnologyClasses} />
-    </div>
+    </>
   )
 }
