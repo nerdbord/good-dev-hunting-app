8a16475e7a1b9c5902e9c6584c9cbb6cd677185f
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-12-17 11:20:05 +0100

    fix: fix

--- cypress/e2e/account_setup_flow.cy.ts
+++ cypress/e2e/account_setup_flow.cy.ts
@@ -1,6 +1,6 @@
 describe('Account Setup', () => {
   it('covers the user golden path', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
     cy.get('[data-testid="createProfileButton"]').should('be.visible').click()
     cy.login()
     cy.wait(5000)
