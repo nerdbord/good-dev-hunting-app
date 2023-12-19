bba03548803d84a0ac77cad9f07af57358937991
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-19 12:32:52 +0100

    fix: changed next congig env vars to match exactly env vars

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
 module.exports = {
   ...nextConfig,
   env: {
-    GITHUB_CLIENT_ID: process.env.GH_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GH_CLIENT_SECRET,
+    GITHUB_ID: process.env.GH_CLIENT_ID,
+    GITHUB_SECRET: process.env.GH_CLIENT_SECRET,
   },
   images: {
     domains: [
