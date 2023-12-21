3896711be3bf79564f3f2fbe7ec067273de06634
AniaPiWo <snowflake@wp.pl>
2023-12-21 11:10:17 +0100

    feat: opengraph,  robots, sitemap

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/*', '/profile/*', '/my-profile/*', '/dashboard/*'],
+      },
+    ],
+    sitemap: `${process.env.NEXTAUTH_URL}/sitemap.xml`,
+  }
+}
