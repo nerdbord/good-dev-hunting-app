c943e99d59a225b8c1f13266968bd756d284995d
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-21 11:46:53 +0100

    fix: moderation btn is now visible on default page if user has moderation role, and on dashboard not

--- src/components/Headers/AppHeader/AppHeader.tsx
+++ src/components/Headers/AppHeader/AppHeader.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
 import logo from '@/assets/images/logo.png'
-import Link from 'next/link'
-import { GithubLoginButton } from '@/components/GithubLoginButton/GithubLoginButton'
-import CreateProfileBtn from '@/components/CreateProfileBtn/CreateProfileBtn'
-import MyProfileBtn from '@/components/MyProfileBtn/MyProfileBtn'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
 import { getProfileByUserEmail } from '@/backend/profile/profile.service'
+import { findUserByEmail } from '@/backend/user/user.service'
 import { Container } from '@/components/Container/Container'
-import styles from './AppHeader.module.scss'
-import { AppRoutes } from '@/utils/routes'
+import CreateProfileBtn from '@/components/CreateProfileBtn/CreateProfileBtn'
 import GithubAcc from '@/components/GithubAcc/GithubAcc'
+import { GithubLoginButton } from '@/components/GithubLoginButton/GithubLoginButton'
+import ModerationBtn from '@/components/ModerationBtn/ModerationBtn'
+import MyProfileBtn from '@/components/MyProfileBtn/MyProfileBtn'
+import { authOptions } from '@/lib/auth'
+import { AppRoutes } from '@/utils/routes'
+import { Role } from '@prisma/client'
+import { getServerSession } from 'next-auth'
+import Link from 'next/link'
+import styles from './AppHeader.module.scss'
 
 const AppHeader = async () => {
   const session = await getServerSession(authOptions)
@@ -18,6 +20,9 @@ const AppHeader = async () => {
   const profile = session
     ? await getProfileByUserEmail(session.user.email)
     : null
+
+  const user = session ? await findUserByEmail(session.user.email) : null
+  const userIsModerator = user?.roles.includes(Role.MODERATOR)
 
   if (session) {
     return (
@@ -29,13 +34,12 @@ const AppHeader = async () => {
               <div className={styles.title}>Good Dev Hunting</div>
             </Link>
             <div className={styles.frameButtons}>
-              <div className={styles.buttonBox}>
-                {profile ? (
-                  <MyProfileBtn />
-                ) : (
-                  <CreateProfileBtn data-testid="create-profile-button" />
-                )}
-              </div>
+              {userIsModerator && <ModerationBtn />}
+              {user?.profile ? (
+                <MyProfileBtn />
+              ) : (
+                <CreateProfileBtn data-testid="create-profile-button" />
+              )}
             </div>
             {profile ? <GithubAcc /> : null}
           </div>
