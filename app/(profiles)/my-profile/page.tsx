bc4f3eb00f2fd087dec8ff1ff41a46406421bbc0
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-01-02 21:02:33 +0100

    feat: move ConnectToNerdbord button to ProfileTopBar

--- app/(profiles)/my-profile/page.tsx
+++ app/(profiles)/my-profile/page.tsx
@@ -29,7 +29,10 @@ const MyProfilePage = async () => {
   return (
     <div className={styles.wrapper}>
       {/* @ts-expect-error Server Component */}
-      <ProfileTopBar profile={profile} />
+      <ProfileTopBar
+        profile={profile}
+        isConnectedToNerdbord={isConnectedToNerdbord}
+      />
       {/* @ts-expect-error Server Component */}
       <ProfileMain
         profile={profile}
