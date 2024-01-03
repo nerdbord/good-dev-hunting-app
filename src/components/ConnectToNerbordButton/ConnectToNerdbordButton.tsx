645295c2eb54799e66a8789fa48f6e7aad4479eb
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-01-03 13:48:12 +0100

    feat: manage the error when user does not exist in nerdbord

--- src/components/ConnectToNerbordButton/ConnectToNerdbordButton.tsx
+++ src/components/ConnectToNerbordButton/ConnectToNerdbordButton.tsx
@@ -2,15 +2,29 @@
 import { connectToNerdbord } from '@/actions/user/connectToNerdobord'
 import { Button } from '../Button/Button'
 import RotateIcon from '@/assets/icons/RotateIcon'
-import { useAsyncAction } from '@/hooks/useAsyncAction'
+import { ToastStatus, useToast } from '@/contexts/ToastContext'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export const ConnectToNerdbordButton = () => {
-  const { runAsync, loading } = useAsyncAction()
+  const { addToast } = useToast()
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
 
-  const handleConnectToNerdbord = () => {
-    runAsync(async () => {
+  const handleConnectToNerdbord = async () => {
+    setLoading(true)
+    try {
       await connectToNerdbord()
-    })
+      router.refresh()
+    } catch (error) {
+      console.error(error)
+      addToast(
+        'User not found. Make sure your Github account is used on Nerdbord',
+        ToastStatus.INVALID,
+      )
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
