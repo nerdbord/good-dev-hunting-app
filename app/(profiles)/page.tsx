88c72ba4fc11cfa26b1616587c78f8109a7e49d9
hansac91 <saczukhanna@gmail.com>
2024-01-11 23:01:17 +0100

    feat:  css changes

--- app/(profiles)/page.tsx
+++ app/(profiles)/page.tsx
@@ -5,24 +5,8 @@ import CreateProfileBtn from '@/components/CreateProfileBtn/CreateProfileBtn'
 import { Button } from '@/components/Button/Button'
 import ProfilePicture from '@/assets/images/ProfilePicture.png'
 import Image from 'next/image'
-import {
-  profileData,
-  JobSpecialization,
-} from '@/components/ProfileList/profile-data'
 
 const Page: React.FC = () => {
-  const getJobSpecializationClass = (jobSpecialization: JobSpecialization) => {
-    return cx({
-      [styles.frontend]: jobSpecialization === JobSpecialization.Frontend,
-      [styles.backend]: jobSpecialization === JobSpecialization.Backend,
-      [styles.fullstack]: jobSpecialization === JobSpecialization.Fullstack,
-    })
-  }
-
-  if (!profileData || profileData.length === 0) {
-    return <div>No data available</div>
-  }
-
   const cx = classNames.bind(styles)
 
   return (
@@ -33,8 +17,8 @@ const Page: React.FC = () => {
           <span>employer find you</span>
         </div>
         <div className={styles.subtitle}>
-          Open sourced, free, hub for software developers to be present and
-          ready for next commission work.
+          <p>Open sourced, free, hub for software developers to be present</p>
+          <p>and ready for next commission work.</p>
         </div>
         <div className={styles.buttons}>
           <CreateProfileBtn />
@@ -42,47 +26,62 @@ const Page: React.FC = () => {
         </div>
       </div>
 
-      {profileData.map((profile) => (
-        <div key={profile.id} className={styles.right}>
-          <div className={styles.frame}>
-            <div className={styles.container}>
-              <div className={styles.profile}>
-                <Image src={ProfilePicture} alt="Profile Picture" />
-              </div>
-              <div className={styles.data}>
-                <p className={styles.name}>{profile.name}</p>
-                <p
-                  className={getJobSpecializationClass(
-                    profile.jobSpecialization,
-                  )}
-                >
-                  {profile.seniority} {profile.jobSpecialization} Developer
-                </p>
-                <p className={styles.location}>
-                  {profile.country}, {profile.city} / {profile.remote}
-                </p>
-              </div>
+      <div className={styles.right}>
+        <div className={styles.frame1}>
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <Image src={ProfilePicture} alt="Profile Picture" />
             </div>
-            <div className={styles.technology}>
-              {profile.technology.map((tech) => (
-                <span
-                  key={tech}
-                  className={getJobSpecializationClass(
-                    profile.jobSpecialization,
-                  )}
-                >
-                  {tech}
-                </span>
-              ))}
-              <span
-                className={getJobSpecializationClass(profile.jobSpecialization)}
-              >
-                {profile.others}
-              </span>
+            <div className={styles.data}>
+              <p className={styles.name}>Karolina Morwinska</p>
+              <p className={styles.fullstack}>Senior Fullstack Developer</p>
+              <p className={styles.location}>Poland, Warsaw / Remote</p>
             </div>
           </div>
+          <div className={`${styles.technology} ${styles.fullstack}`}>
+            <span>Javascript</span>
+            <span>React</span>
+            <span>Vue.js</span>
+            <span>+5 more</span>
+          </div>
         </div>
-      ))}
+        <div className={styles.frame2}>
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <Image src={ProfilePicture} alt="Profile Picture" />
+            </div>
+            <div className={styles.data}>
+              <p className={styles.name}>Kristin Watson</p>
+              <p className={styles.frontend}>Junior Frontend Developer</p>
+              <p className={styles.location}>Poland, Warsaw / Remote</p>
+            </div>
+          </div>
+          <div className={`${styles.technology} ${styles.frontend}`}>
+            <span>Javascript</span>
+            <span>React</span>
+            <span>Vue.js</span>
+            <span>+5 more</span>
+          </div>
+        </div>
+        <div className={styles.frame3}>
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <Image src={ProfilePicture} alt="Profile Picture" />
+            </div>
+            <div className={styles.data}>
+              <p className={styles.name}>Veres Panna</p>
+              <p className={styles.fullstack}>Mid Fullstack Developer</p>
+              <p className={styles.location}>Poland, Warsaw / Remote</p>
+            </div>
+          </div>
+          <div className={`${styles.technology} ${styles.fullstack}`}>
+            <span>Javascript</span>
+            <span>React</span>
+            <span>Vue.js</span>
+            <span>+5 more</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
