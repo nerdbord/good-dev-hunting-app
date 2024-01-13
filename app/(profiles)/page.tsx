a1d57fe7bc95acc6bcd543996abcbb3207e09e53
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-01-13 14:52:19 +0100

    feat: landing FAQ section - add & style FAQ accordion

--- app/(profiles)/page.tsx
+++ app/(profiles)/page.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Filters from '@/components/Filters/Filters'
 import ProfileList from '@/components/ProfileList/ProfileList'
 import { FiltersProvider } from '@/contexts/FilterContext'
-import FAQ from '@/components/LandingPage/FAQ/FAQ'
 
 export default function Home() {
   return (
     <FiltersProvider>
-      {/* <Filters /> */}
-      {/* <ProfileList /> */}
-      <FAQ />
+      <Filters />
+      {/* @ts-expect-error Server Component */}
+      <ProfileList />
     </FiltersProvider>
   )
 }
---
+++ src/components/LandingPage/FAQ/Accordion/Accordion.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { ReactNode, useState } from 'react'
+import styles from './Accordion.module.scss'
+import AccordionIcon from './icons/AccordionIcon'
+import { AccordionIconType } from '../enums'
+
+interface AccordionProps {
+  title: string
+  children: ReactNode
+}
+
+const Accordion = (props: AccordionProps) => {
+  const [accordionOpen, setAccordionOpen] = useState(false)
+
+  const handleOpen = () => {
+    setAccordionOpen((prevOpen) => !prevOpen)
+  }
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.titleWrapper} onClick={handleOpen}>
+        <p>{props.title}</p>
+        <div className={styles.iconWrapper}>
+          <AccordionIcon
+            type={
+              accordionOpen ? AccordionIconType.Subtract : AccordionIconType.Add
+            }
+          />
+        </div>
+      </div>
+      {accordionOpen && (
+        <div className={styles.accordionContent}>{props.children}</div>
+      )}
+    </section>
+  )
+}
+
+export default Accordion
