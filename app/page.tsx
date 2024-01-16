b4bc7c239175e5f56831b7d96fa53bac562927e7
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2024-01-16 11:51:02 +0100

    fix: delete "use client" from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-'use client'
 import React from 'react'
 import styles from './page.module.scss'
 import { Container } from '@/components/Container/Container'
 import Hero from '@/components/landing-page/Hero/Hero'
 import HowItWorks from '@/components/landing-page/HowItWorks/HowItWorks'
-import AppHeader from '@/components/Headers/AppHeader/AppHeader'
 
 const Page: React.FC = () => {
   return (
