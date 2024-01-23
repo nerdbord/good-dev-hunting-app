f2732961effd50420c94357d3c3edcbb5fd5bc57
The Fox <lisieckikamil93@gmail.com>
2024-01-23 16:41:40 +0100

    fix: 'profile not found' displays correctly

--- src/components/ProfileList/ProfileList.tsx
+++ src/components/ProfileList/ProfileList.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import styles from '@/components/ProfileList/ProfileList.module.scss'
 import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
 import ProfilesWithFilter from '@/components/ProfileList/ProfilesWithFilter'
-import { Container } from '@/components/Container/Container'
 
 const ProfileList = async () => {
   const profiles = await getPublishedProfilesPayload()
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.profileListCont}>
-        <ProfilesWithFilter data={profiles} />
-      </div>
+    <div className={styles.profileListCont}>
+      <ProfilesWithFilter data={profiles} />
     </div>
   )
 }
