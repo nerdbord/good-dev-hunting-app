2fdd7b0c2fb44deacfe1d8581b9e33e058f69346
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-01-23 17:25:27 +0100

    fix: fix

--- src/components/ProfileList/ProfileList.tsx
+++ src/components/ProfileList/ProfileList.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import styles from '@/components/ProfileList/ProfileList.module.scss'
 import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
 import ProfilesWithFilter from '@/components/ProfileList/ProfilesWithFilter'
 
 const ProfileList = async () => {
   const profiles = await getPublishedProfilesPayload()
 
-  return (
-    <div className={styles.profileListCont}>
-      <ProfilesWithFilter data={profiles} />
-    </div>
-  )
+  return <ProfilesWithFilter data={profiles} />
 }
 export default ProfileList
