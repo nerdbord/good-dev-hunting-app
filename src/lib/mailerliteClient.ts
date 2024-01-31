022b203396865f35017b8c50cbe1cde66b16939b
Szymon Kadaś <kadasszymon@gmail.com>
2024-01-31 22:48:27 +0100

    refactor: mailerLite client has now addSubscriberToMailerLite and fetchSubscribers

--- src/lib/mailerliteClient.ts
+++ src/lib/mailerliteClient.ts
@@ -1,9 +1,37 @@
 import MailerLite from '@mailerlite/mailerlite-nodejs'
+import { httpClient } from './httpClient'
 
-export const mailerLiteClient = new MailerLite({
+const mailerliteInstance = new MailerLite({
   api_key: process.env.NEXT_PUBLIC_MAILERLITE_KEY || '',
 })
 
-export const devGroup = '111547703431792014'
-
-export const contactGroup = '111547722557818681'
+export const mailerliteClient = {
+  devGroup: '111547703431792014',
+  contactGroup: '111547722557818681',
+  addSubscriberToMailerLite: async (
+    email: string,
+    groupId: string,
+    fields?: Record<string, string>,
+  ) => {
+    try {
+      return await mailerliteInstance.subscribers.createOrUpdate({
+        email,
+        groups: [groupId],
+        fields,
+      })
+    } catch (error) {
+      console.error('Error adding subscriber to MailerLite:', error)
+      return error
+    }
+  },
+  fetchSubscribersFromMailerLite: async (groupId: string) => {
+    try {
+      return await httpClient.get(
+        `https://connect.mailerlite.com/api/groups/${groupId}/subscribers`,
+      )
+    } catch (error) {
+      console.error('Error fetching subscribers', error)
+      return error
+    }
+  },
+}
