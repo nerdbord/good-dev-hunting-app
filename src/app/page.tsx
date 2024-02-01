a61853131572cca42ee9389a281a1563693cd26f
Szymon Kadaś <kadasszymon@gmail.com>
2024-02-01 13:20:20 +0100

    feat: team members are now listed on meet our team, todo: figure out how to display employment type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import styles from './page.module.scss'
-import { LandingContainer } from '@/components/landing-page/LandingContainer/LandingContainer'
-import LandingHeader from '@/components/landing-page/LandingHeader/LandingHeader'
+import { Container } from '@/components/Container/Container'
+import FAQSection from '@/components/landing-page/FAQSection/FAQSection'
 import Hero from '@/components/landing-page/Hero/Hero'
 import HowItWorks from '@/components/landing-page/HowItWorks/HowItWorks'
-import UseYourProfile from '@/components/landing-page/UseYourProfile/UseYourProfile'
+import LandingFooter from '@/components/landing-page/LandingFooter/LandingFooter'
+import LandingHeader from '@/components/landing-page/LandingHeader/LandingHeader'
 import MeetTeam from '@/components/landing-page/MeetTeam/MeetTeam'
 import TalentSection from '@/components/landing-page/TalentSection/TalentSection'
-import FAQSection from '@/components/landing-page/FAQSection/FAQSection'
-import LandingFooter from '@/components/landing-page/LandingFooter/LandingFooter'
-import { Container } from '@/components/Container/Container'
+import UseYourProfile from '@/components/landing-page/UseYourProfile/UseYourProfile'
+import React from 'react'
+import styles from './page.module.scss'
 
 const Page: React.FC = () => {
   return (
