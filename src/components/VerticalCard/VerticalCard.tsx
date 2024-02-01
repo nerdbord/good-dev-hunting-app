0bdf3b49d445a0c99ce28d3d6e29d54ea2a861ed
Szymon Kadaś <kadasszymon@gmail.com>
2024-02-01 17:09:09 +0100

    refactor: simplified MeetTeam, VerticalCard done

--- src/components/VerticalCard/VerticalCard.tsx
+++ src/components/VerticalCard/VerticalCard.tsx
@@ -1,23 +1,28 @@
 import ProfilePicture from '@/assets/images/ProfilePicture.png'
 import { mapSeniorityLevel } from '@/data/frontend/profile/mappers'
-import { JobSpecialization, ProfileModel } from '@/data/frontend/profile/types'
-import classNames from 'classnames/bind'
+import { ProfileModel } from '@/data/frontend/profile/types'
+import { EmploymentType } from '@prisma/client'
 import Image from 'next/image'
 import styles from './VerticalCard.module.scss'
 
-const cx = classNames.bind(styles)
-
-const VerticalCard = ({ profile }: { profile?: ProfileModel }) => {
-  const commonClasses = {
-    [styles.frontend]: profile?.position === JobSpecialization.Frontend,
-    [styles.backend]: profile?.position === JobSpecialization.Backend,
-    [styles.fullstack]: profile?.position === JobSpecialization.Fullstack,
-  }
-
-  const getStackClasses = cx(commonClasses)
-  const getTechnologyClasses = cx({
-    [styles.technology]: true,
-    ...commonClasses,
+const VerticalCard = ({
+  position,
+  techStack,
+  city,
+  country,
+  seniority,
+  fullName,
+  remoteOnly,
+  employmentTypes,
+}: ProfileModel) => {
+  const technologies = techStack.map((tech, index) => {
+    if (index < 4) {
+      return (
+        <p className={styles.tech}>
+          {index < 3 ? tech.name : `+ ${techStack.length - index} more`}
+        </p>
+      )
+    }
   })
   return (
     <div className={styles.card}>
@@ -29,44 +34,22 @@ const VerticalCard = ({ profile }: { profile?: ProfileModel }) => {
         className={styles.avatar}
       />
       <div className={styles.person}>
-        <h4 className={styles.name}>
-          {profile?.fullName || 'Karolina Morwińska'}
-        </h4>
+        <h4 className={styles.name}>{fullName}</h4>
         <div className={styles.position}>
-          {profile?.seniority
-            ? mapSeniorityLevel(profile?.seniority)
-            : 'Senior'}{' '}
-          {profile?.position || 'Fullstack Developer'}
+          {mapSeniorityLevel(seniority)} {position}
         </div>
         <div className={styles.location}>
-          {profile?.country.name || 'Poland'}, {profile?.city.name || 'Warsaw'}{' '}
-          {profile?.remoteOnly && '/ Remote'}
+          {country.name}, {city.name} {remoteOnly && '/ Remote'}
         </div>
       </div>
-      <div className={styles.techStack}>
-        {profile?.techStack.map((tech, index) => {
-          if (index < 4) {
-            return (
-              <p className={styles.tech}>
-                {index < 3
-                  ? tech.name
-                  : `+ ${profile.techStack.length - index} more`}
-              </p>
-            )
-          }
-        }) || (
-          <>
-            <p className={styles.tech}>Javascript</p>
-            <p className={styles.tech}>React.js</p>
-            <p className={styles.tech}>Vue.js</p>
-            <p className={styles.tech}>+5 more</p>
-          </>
-        )}
-      </div>
+      <div className={styles.techStack}>{...technologies}</div>
       <div className={styles.availability}>
-        {/* Dotsy dostosować pod part time/full time/contract ? Jak tutaj wyświetlać */}
         <div className={styles.dot}></div>
-        {profile?.employmentTypes ? '' : 'Available for full-time'}
+        {employmentTypes.includes(EmploymentType.FULL_TIME)
+          ? 'Available for full-time'
+          : employmentTypes.includes(EmploymentType.CONTRACT)
+          ? 'Available for a contract'
+          : 'Available for part-time'}
       </div>
     </div>
   )
