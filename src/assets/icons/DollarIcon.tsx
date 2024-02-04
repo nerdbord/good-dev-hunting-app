75dac1b00aaa76c736f49a7888d7f860fb97f81c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-02-05 00:01:35 +0100

    fix: fix typo

--- src/assets/icons/DollarIcon.tsx
+++ src/assets/icons/DollarIcon.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const DollarIcon = () => {
   return (
     <svg
@@ -12,20 +10,20 @@ const DollarIcon = () => {
       <path
         d="M17 5.66675V8.50008"
         stroke="#A687FF"
-        stroke-width="1.41667"
-        stroke-linecap="round"
+        strokeWidth="1.41667"
+        strokeLinecap="round"
       />
       <path
         d="M17 25.5V28.3333"
         stroke="#A687FF"
-        stroke-width="1.41667"
-        stroke-linecap="round"
+        strokeWidth="1.41667"
+        strokeLinecap="round"
       />
       <path
         d="M21.9583 11.3333C21.9583 11.3333 21.25 8.5 17 8.5C12.75 8.5 12.0417 11.2717 12.0417 12.75C12.0417 18.6155 21.9583 15.5337 21.9583 21.25C21.9583 22.7283 21.25 25.5 17 25.5C12.75 25.5 12.0417 22.6667 12.0417 22.6667"
         stroke="#A687FF"
-        stroke-width="1.41667"
-        stroke-linecap="round"
+        strokeWidth="1.41667"
+        strokeLinecap="round"
       />
     </svg>
   )
