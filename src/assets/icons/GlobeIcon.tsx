196feb40ade59e06b60e7e3a6b4643969b162a02
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-02-05 00:01:35 +0100

    fix: fix typo

--- src/assets/icons/GlobeIcon.tsx
+++ src/assets/icons/GlobeIcon.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const GlobeIcon = () => {
   return (
     <svg
@@ -10,30 +8,30 @@ const GlobeIcon = () => {
       fill="none"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M17 31.1666C24.824 31.1666 31.1666 24.824 31.1666 16.9999C31.1666 9.17588 24.824 2.83325 17 2.83325C9.17595 2.83325 2.83331 9.17588 2.83331 16.9999C2.83331 24.824 9.17595 31.1666 17 31.1666Z"
         stroke="#A687FF"
-        stroke-width="1.41667"
+        strokeWidth="1.41667"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M17 29.75C17 29.75 22.6666 25.1136 22.6666 17C22.6666 8.88636 17 4.25 17 4.25C17 4.25 11.3333 8.88636 11.3333 17C11.3333 25.1136 17 29.75 17 29.75Z"
         stroke="#A687FF"
-        stroke-width="1.41667"
+        strokeWidth="1.41667"
       />
       <path
         d="M3.54165 12.7499H30.4583"
         stroke="#A687FF"
-        stroke-width="1.41667"
-        stroke-linecap="round"
+        strokeWidth="1.41667"
+        strokeLinecap="round"
       />
       <path
         d="M3.54165 21.2499H30.4583"
         stroke="#A687FF"
-        stroke-width="1.41667"
-        stroke-linecap="round"
+        strokeWidth="1.41667"
+        strokeLinecap="round"
       />
     </svg>
   )
