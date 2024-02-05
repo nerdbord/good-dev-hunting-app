76e2cb7cf7862b87d09530327d1ab761cb122eec
Jakub Wąsowski <jakub@nerdz.pl>
2024-02-05 17:23:14 +0100

    fix: fix hero

--- src/components/landing-page/Hero/Hero.tsx
+++ src/components/landing-page/Hero/Hero.tsx
@@ -2,18 +2,15 @@ import { authOptions } from '@/app/(auth)/auth'
 import MyProfileBtn from '@/app/(profile)/(components)/MyProfileBtn/MyProfileBtn'
 import { ProfileCardWrapper } from '@/app/(profile)/(components)/ProfileCard/ProfileCardWrapper/ProfileCardWrapper'
 import CreateProfileBtn from '@/app/(profile)/my-profile/(components)/CreateProfileBtn/CreateProfileBtn'
-import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
+import { getRandomProfiles } from '@/backend/profile/profile.service'
 import { findUserByEmail } from '@/backend/user/user.service'
 import FindTalentsBtn from '@/components/FindTalentsBtn/FindTalentsBtn'
-import { getRandomIndexes } from '@/utils/getRandomProfiles'
 import { getServerSession } from 'next-auth'
 import styles from './Hero.module.scss'
 const Hero = async () => {
   const session = await getServerSession(authOptions)
-  const profiles = await getPublishedProfilesPayload()
   const user = session ? await findUserByEmail(session.user.email) : null
-
-  const randomIndexes = getRandomIndexes(profiles.length, 3)
+  const [first, second, third] = await getRandomProfiles(3)
 
   return (
     <section id="hero" className={styles.wrapper}>
@@ -27,22 +24,28 @@ const Hero = async () => {
           <p>and ready for next commission work.</p>
         </div>
         <div className={styles.buttons}>
-          {user?.profile ? <MyProfileBtn /> : <CreateProfileBtn />}
+          {user ? <MyProfileBtn /> : <CreateProfileBtn />}
           <FindTalentsBtn variant={'secondary'}>Find talents</FindTalentsBtn>
         </div>
       </div>
 
       <div className={styles.right}>
         <div className={styles.section}>
-          <div className={styles.frame1}>
-            <ProfileCardWrapper profile={profiles[randomIndexes[0]]} />
-          </div>
-          <div className={styles.frame2}>
-            <ProfileCardWrapper profile={profiles[randomIndexes[1]]} />
-          </div>
-          <div className={styles.frame3}>
-            <ProfileCardWrapper profile={profiles[randomIndexes[2]]} />
-          </div>
+          {first && (
+            <div className={styles.frame1}>
+              <ProfileCardWrapper profile={first} />
+            </div>
+          )}
+          {second && (
+            <div className={styles.frame2}>
+              <ProfileCardWrapper profile={second} />
+            </div>
+          )}
+          {third && (
+            <div className={styles.frame3}>
+              <ProfileCardWrapper profile={third} />
+            </div>
+          )}
         </div>
       </div>
     </section>
