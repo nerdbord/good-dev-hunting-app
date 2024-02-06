3c906a7df8235b5876cfe2a32b4aba95c080e22f
wawrzonnn <norbert.wawrzonek@gmail.com>
2024-02-06 16:39:35 +0100

    fix: filter only by approved location

--- src/backend/country/country.service.ts
+++ src/backend/country/country.service.ts
@@ -1,7 +1,15 @@
 import { prisma } from '@/lib/prismaClient'
+import { PublishingState } from '@prisma/client'
 
 export async function getCountries() {
   return await prisma.country.findMany({
+    where: {
+      profile: {
+        some: {
+          state: PublishingState.APPROVED,
+        },
+      },
+    },
     orderBy: {
       name: 'asc',
     },
