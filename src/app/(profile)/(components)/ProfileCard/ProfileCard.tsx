b5ea1d42aaad487199ab011181b439920afa2f45
wawrzonnn <norbert.wawrzonek@gmail.com>
2024-02-07 00:19:23 +0100

    feat: added avatar component

--- src/app/(profile)/(components)/ProfileCard/ProfileCard.tsx
+++ src/app/(profile)/(components)/ProfileCard/ProfileCard.tsx
@@ -1,9 +1,9 @@
 import { mapEmploymentTypes, mapSeniorityLevel } from '@/app/(profile)/mappers'
 import { StateStatus } from '@/app/(profile)/moderation/(components)/StateStatus/StateStatus'
 import { JobSpecialization, ProfileModel } from '@/app/(profile)/types'
+import { Avatar } from '@/components/Avatar/Avatar'
 import TechnologiesRenderer from '@/components/renderers/TechnologiesRenderer'
 import classNames from 'classnames/bind'
-import Image from 'next/image'
 import styles from './ProfileCard.module.scss'
 
 interface ProfileCardProps {
@@ -35,13 +35,7 @@ const ProfileCard = ({ data, onClick, withStateStatus }: ProfileCardProps) => {
       <div className={styles.frame} onClick={onClick}>
         <div className={styles.container} data-test-id="profileContainer">
           <div className={styles.profile}>
-            <Image
-              src={data.avatarUrl || ''}
-              width={78}
-              height={78}
-              alt="user's avatar"
-              className={styles.avatar}
-            />
+            <Avatar src={data.avatarUrl || ''} size={78} />
           </div>
           <div className={styles.data}>
             <p className={styles.name}>{data.fullName}</p>
