7f0ea0e7d4399a691ce994d3aeeb72f7ef3c979e
Jakub Wąsowski <jakub@nerdz.pl>
2024-02-07 17:23:07 +0100

    feat: improve notifications

--- src/app/(profile)/_actions/rejectProfile.ts
+++ src/app/(profile)/_actions/rejectProfile.ts
@@ -2,6 +2,7 @@
 import { authorizeUser } from '@/app/(auth)/auth'
 import { sendProfileRejectedEmail } from '@/backend/mailing/mailing.service'
 import {
+  findGithubUsernameByProfileId,
   getProfileById,
   updateProfileById,
 } from '@/backend/profile/profile.service'
@@ -9,6 +10,7 @@ import {
   deleteRejectingReason,
   saveRejectingReason,
 } from '@/backend/profile/rejection.service'
+import { findUserByEmail } from '@/backend/user/user.service'
 import { sendDiscordNotificationToModeratorChannel } from '@/lib/discord'
 import { requireUserRoles } from '@/utils/auths'
 import { withSentry } from '@/utils/errHandling'
@@ -16,7 +18,7 @@ import { PublishingState, Role } from '@prisma/client'
 
 export const rejectProfile = withSentry(
   async (profileId: string, reason: string) => {
-    await authorizeUser()
+    const { email } = await authorizeUser()
 
     const isModerator = await requireUserRoles([Role.MODERATOR])
 
@@ -29,15 +31,22 @@ export const rejectProfile = withSentry(
       throw new Error('Rejection failed, profile not found.')
     }
 
+    const moderator = await findUserByEmail(email)
     const createdReason = await saveRejectingReason(profileId, reason)
+    const profileOwnerUsername = await findGithubUsernameByProfileId(profileId)
 
     const updatedProfile = await updateProfileById(profile.id, {
       state: PublishingState.REJECTED,
     })
+
     try {
       await sendProfileRejectedEmail(profile?.userEmail, reason)
       await sendDiscordNotificationToModeratorChannel(
-        `User's **${updatedProfile.fullName}** profile has got new status: **${updatedProfile.state}**! [Show Profile](${process.env.NEXT_PUBLIC_APP_ORIGIN_URL}/moderation/profile/${updatedProfile.userId})`,
+        `${
+          moderator?.profile?.fullName || 'Moderator'
+        } rejected ${profileOwnerUsername} profile. [Show Profile](${
+          process.env.NEXT_PUBLIC_APP_ORIGIN_URL
+        }/moderation/profile/${updatedProfile.userId})`,
       )
       return updatedProfile
     } catch (error) {
