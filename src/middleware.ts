54bee67d9b0d2d4b2f41b837557f15c0fd80f7b9
Jakub Wąsowski <jakub@nerdz.pl>
2024-02-08 20:21:22 +0100

    chore: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request: any) {
+  const basicAuth = request.headers.get('authorization')
+
+  const USERNAME = process.env.BASIC_AUTH_USERNAME
+  const PASSWORD = process.env.BASIC_AUTH_PASSWORD
+
+  const authString = `Basic ${Buffer.from(`${USERNAME}:${PASSWORD}`).toString(
+    'base64',
+  )}`
+
+  const isStaging = process.env.NODE_ENV === 'test'
+
+  if (!isStaging) {
+    return NextResponse.next()
+  }
+
+  if (basicAuth === authString) {
+    // Authentication successful
+    return NextResponse.next()
+  } else {
+    // Construct a response for the unauthorized request
+    const response = new Response('Authentication required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    })
+
+    return response
+  }
+}
