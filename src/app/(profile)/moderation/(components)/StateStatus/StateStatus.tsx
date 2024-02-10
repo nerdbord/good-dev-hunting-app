507c398b1da2a7af7c9c802febe185f58713bccf
Jakub Wąsowski <jakub@nerdz.pl>
2024-02-10 20:57:21 +0100

    fix: cleanup

--- src/app/(profile)/moderation/(components)/StateStatus/StateStatus.tsx
+++ src/app/(profile)/moderation/(components)/StateStatus/StateStatus.tsx
@@ -1,3 +1,4 @@
+import { formatStateName } from '@/app/(profile)/(components)/FilterTabs/Tab'
 import { approveProfile } from '@/app/(profile)/_actions/approveProfile'
 import RejectingReasonModal from '@/app/(profile)/moderation/(components)/RejectingReasonModal/RejectingReasonModal'
 import { ProfileModel } from '@/app/(profile)/types'
@@ -42,24 +43,62 @@ export function StateStatus({ profile }: StateStatusProps) {
   const isApproved = state === PublishingState.APPROVED
   const isRejected = state === PublishingState.REJECTED
 
+  const formattedName = formatStateName(state)
+
   return (
     <div className={styles.actions}>
-      <Button
-        variant="action"
-        loading={loading}
-        onClick={handleApprove}
-        disabled={!isRejected && !isPending}
-      >
-        Accept <AcceptIcon />
-      </Button>
-      <Button
-        variant="action"
-        loading={loading}
-        onClick={handleShowRejectModal}
-        disabled={!isApproved && !isPending} // modified condition
-      >
-        Reject <RejectIcon />
-      </Button>
+      {!isPending && (
+        <div
+          className={cx({
+            [styles.stateStatus]: true,
+            [styles.stateAccepted]: state === PublishingState.APPROVED,
+            [styles.stateRejected]: state === PublishingState.REJECTED,
+          })}
+        >
+          {formattedName}
+        </div>
+      )}
+      {isPending && (
+        <>
+          <Button
+            variant="action"
+            loading={loading}
+            onClick={handleApprove}
+            disabled={!isRejected && !isPending}
+          >
+            Accept <AcceptIcon />
+          </Button>
+          <Button
+            variant="action"
+            loading={loading}
+            onClick={handleShowRejectModal}
+            disabled={!isApproved && !isPending} // modified condition
+          >
+            Reject <RejectIcon />
+          </Button>
+        </>
+      )}
+      {isRejected && (
+        <Button
+          variant="action"
+          loading={loading}
+          onClick={handleApprove}
+          disabled={!isRejected && !isPending}
+        >
+          Accept <AcceptIcon />
+        </Button>
+      )}
+
+      {isApproved && (
+        <Button
+          variant="action"
+          loading={loading}
+          onClick={handleShowRejectModal}
+          disabled={!isApproved && !isPending} // modified condition
+        >
+          Reject <RejectIcon />
+        </Button>
+      )}
     </div>
   )
 }
