e673afc2911f7a3574a28812b09b262f01a17e09
The Fox <lisieckikamil93@gmail.com>
2024-02-12 17:16:21 +0100

    feat: change sorting to drawer for in collection to be more safety

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,5 +1,9 @@
-const shuffleArray = <T>(array: T[]): T[] => {
-  return array.sort(() => Math.random() - 0.5)
+function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
 }
 
 export default shuffleArray
