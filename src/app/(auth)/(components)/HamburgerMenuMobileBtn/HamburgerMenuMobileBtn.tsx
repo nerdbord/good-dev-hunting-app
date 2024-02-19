408b6ba1c9bb8db1a93dc29d7bba8a3c7ffaf7c6
The Fox <lisieckikamil93@gmail.com>
2024-02-19 22:52:33 +0100

    feat(HamburgerMenuMobileBtn): add 'standard' styles to btn for mobile hamburger menu

--- src/app/(auth)/(components)/HamburgerMenuMobileBtn/HamburgerMenuMobileBtn.tsx
+++ src/app/(auth)/(components)/HamburgerMenuMobileBtn/HamburgerMenuMobileBtn.tsx
@@ -1,9 +1,18 @@
+'use client'
 import { Button } from '@/components/Button/Button'
+import { useState } from 'react'
 
 const HamburgerMenuMobileBtn = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+
+  const handleOpenMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     <div>
-      <Button variant="primary">Menu</Button>
+      <Button onClick={handleOpenMenu} variant="standard">
+        {isMenuOpen ? 'X' : 'Menu'}
+      </Button>
     </div>
   )
 }
