51b6bcb68ca5431afaddffaf42302add72e28de2
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-02-20 23:31:40 +0100

    feat: create login page UI - css fix

--- src/app/(auth)/login/(components)/LoginHunter/LoginHunter.tsx
+++ src/app/(auth)/login/(components)/LoginHunter/LoginHunter.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react'
+
 // components
 import Box from '@/app/(auth)/login/(components)/Box/Box'
 import { Button } from '@/components/Button/Button'
@@ -7,33 +9,37 @@ import CheckboxInput from '@/components/Checkbox/Checkbox'
 import TextInput from '@/components/TextInput/TextInput'
 
 const LoginHunter = () => {
+  const [isChecked, setIsChecked] = useState(false)
   return (
     <Box>
       <h2>Login as talent Hunter</h2>
       <p>
-        Join and login alaways with magic link. Hunters don’t have public
+        Join and login alaways with magic link. Hunters don't have public
         profiles.
       </p>
-      <TextInput
-        label="Email"
-        tooltipText="More info"
-        addImportantIcon={true}
-        placeholder="eg. peter.parker@oscorp.com"
-        onChange={() => {
-          console.log('')
-        }}
-      />
-      {/* checkbox */}
-      <CheckboxInput
-        id={'policy'}
-        label={'I have read and accept T&C and Privacy Policy'}
-        checked={false}
-        onChange={() => {
-          console.log('')
-        }}
-        name={'policy'}
-      />
-      <Button variant={'primary'}>Join as a Hunter</Button>
+      <form>
+        <TextInput
+          label="Email"
+          tooltipText=" Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+          addImportantIcon={true}
+          placeholder="eg. peter.parker@oscorp.com"
+          onChange={() => {
+            console.log('')
+          }}
+        />
+        <div style={{ marginBlock: '24px' }}>
+          <CheckboxInput
+            id={'terms'}
+            label={'I have read and accept T&C and Privacy Policy'}
+            checked={isChecked}
+            onChange={() => {
+              setIsChecked((prevState) => !prevState)
+            }}
+            name={'terms'}
+          />
+        </div>
+        <Button variant={'primary'}>Join as a Hunter</Button>
+      </form>
     </Box>
   )
 }
