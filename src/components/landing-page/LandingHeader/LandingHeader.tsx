933f37992141ead09b1e8f589fae50e96070ac7d
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-02-24 15:05:36 +0100

    feat: adjust login page UI to mobile - AppHeader: use getAuthorizedUser & add Login btns

--- src/components/landing-page/LandingHeader/LandingHeader.tsx
+++ src/components/landing-page/LandingHeader/LandingHeader.tsx
@@ -1,22 +1,16 @@
 import { GithubLoginButton } from '@/app/(auth)/(components)/GithubLoginButton/GithubLoginButton'
-import { authOptions } from '@/app/(auth)/auth'
+import { getAuthorizedUser } from '@/app/(auth)/auth'
 import MyProfileBtn from '@/app/(profile)/(components)/MyProfileBtn/MyProfileBtn'
 import CreateProfileBtn from '@/app/(profile)/my-profile/(components)/CreateProfileBtn/CreateProfileBtn'
-import { findUserByEmail } from '@/backend/user/user.service'
 import GithubStarsButton from '@/components/Button/GitHubStarsBtn'
 import { Container } from '@/components/Container/Container'
 import FindTalentsBtn from '@/components/FindTalentsBtn/FindTalentsBtn'
-import LoginBtn from '@/components/LoginBtn/LoginBtn'
+import LoginBtnsWrapper from '@/components/LoginBtn/LoginBtnsWrapper'
 import Logo from '@/components/Logo/Logo'
-import { getServerSession } from 'next-auth'
 import styles from './LandingHeader.module.scss'
 
 const LandingHeader = async () => {
-  const session = await getServerSession(authOptions)
-
-  const user = session?.user?.email
-    ? await findUserByEmail(session.user.email)
-    : null
+  const { session, user } = await getAuthorizedUser()
 
   if (session) {
     return (
@@ -53,9 +47,7 @@ const LandingHeader = async () => {
             <GithubStarsButton />
             <div className={styles.buttonBoxDesktop}>
               <FindTalentsBtn variant="tertiary" />
-              <LoginBtn variant="secondary">Login</LoginBtn>
-              <div className={styles.btnSeparator}></div>
-              <LoginBtn variant="secondary">Join as Hunter</LoginBtn>
+              <LoginBtnsWrapper />
               <CreateProfileBtn />
             </div>
             <div className={styles.buttonBoxMobile}>
