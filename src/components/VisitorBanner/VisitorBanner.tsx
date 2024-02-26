5daa0d7135394501fa9ea4640a461955541d0bea
SzDabrowski <szdabrowsky@gmail.com>
2024-02-26 16:38:26 +0100

    fix: return null instead of empty element

--- src/components/VisitorBanner/VisitorBanner.tsx
+++ src/components/VisitorBanner/VisitorBanner.tsx
@@ -5,19 +5,17 @@ import styles from './VisitorBanner.module.scss'
 const VisitorBanner = async () => {
   const session = await getServerSession(authOptions)
 
-  return (
-    <>
-      {!session?.user ? (
-        <div className={styles.wrapper}>
-          <span>
-            Create a free account to get unlimited access to all profiles
-          </span>
-        </div>
-      ) : (
-        <></>
-      )}
-    </>
-  )
+  if (!session?.user) {
+    return (
+      <div className={styles.wrapper}>
+        <span>
+          Create a free account to get unlimited access to all profiles
+        </span>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 export default VisitorBanner
