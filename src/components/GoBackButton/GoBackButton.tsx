a626aa329eb0c4d2934531c4fb77072bb8238e9e
wawrzonnn <norbert.wawrzonek@gmail.com>
2024-02-27 21:44:45 +0100

    fix: add profiles counter

--- src/components/GoBackButton/GoBackButton.tsx
+++ src/components/GoBackButton/GoBackButton.tsx
@@ -1,13 +1,16 @@
 'use client'
-import React, { PropsWithChildren } from 'react'
-import styles from './GoBackButton.module.scss'
+import { AppRoutes } from '@/utils/routes'
 import { useRouter } from 'next/navigation'
+import { PropsWithChildren } from 'react'
+import styles from './GoBackButton.module.scss'
 
 export const GoBackButton = ({ children }: PropsWithChildren<object>) => {
   const router = useRouter()
 
   const handleClick = () => {
-    router.back()
+    !document.referrer || !document.referrer.includes(window.location.hostname)
+      ? router.push(AppRoutes.profiles)
+      : router.back()
   }
 
   return (
