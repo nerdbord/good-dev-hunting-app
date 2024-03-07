6d1c2de342ea2b8124cd65e863c09ac7378c1bf9
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-03-07 11:50:48 +0100

    fix: show loading state when filtering

--- src/app/(profile)/profiles/page.tsx
+++ src/app/(profile)/profiles/page.tsx
@@ -1,36 +1,18 @@
 import { FiltersWithData } from '@/app/(profile)/(components)/Filters/FiltersWithData'
-import {
-  filterByAvailability,
-  filterByLocation,
-  filterByPosition,
-  filterBySeniority,
-  filterByTechnology,
-} from '@/app/(profile)/(components)/ProfileList/filters'
-import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
-
 import ProfileList from '@/app/(profile)/(components)/ProfileList/ProfileList'
 import { type JobOfferFiltersEnum } from '@/app/(profile)/types'
 import Loader from '@/components/Loader/Loader'
 import { Suspense } from 'react'
 
-export default async function Home({
+export default function Home({
   searchParams,
 }: {
   searchParams: Record<JobOfferFiltersEnum, string>
 }) {
-  const profiles = await getPublishedProfilesPayload()
-
-  const filteredProfiles = profiles
-    .filter(filterByPosition(searchParams.position?.split(',')))
-    .filter(filterBySeniority(searchParams.seniority?.split(',')))
-    .filter(filterByLocation(searchParams.location?.split(',')))
-    .filter(filterByTechnology(searchParams.technology?.split(',')))
-    .filter(filterByAvailability(searchParams.availability?.split(',')))
-
   return (
-    <Suspense fallback={<Loader />}>
+    <Suspense key={JSON.stringify(searchParams)} fallback={<Loader />}>
       <FiltersWithData filters={searchParams} />
-      <ProfileList profiles={filteredProfiles} />
+      <ProfileList filters={searchParams} />
     </Suspense>
   )
 }
