0c08b3fbd8e7aaacef313b78a0da0f3a5010193f
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-03-07 11:50:48 +0100

    fix: show loading state when filtering

--- src/app/(profile)/(components)/ProfileList/ProfileList.tsx
+++ src/app/(profile)/(components)/ProfileList/ProfileList.tsx
@@ -1,14 +1,34 @@
 import { ProfileListItem } from '@/app/(profile)/(components)/ProfileList/ProfileListItem'
-import { type ProfileModel } from '@/app/(profile)/types'
+import {
+  filterByAvailability,
+  filterByLocation,
+  filterByPosition,
+  filterBySeniority,
+  filterByTechnology,
+} from '@/app/(profile)/(components)/ProfileList/filters'
+import { type JobOfferFiltersEnum } from '@/app/(profile)/types'
+import { getPublishedProfilesPayload } from '@/backend/profile/profile.service'
 import styles from './ProfileList.module.scss'
 
-const ProfileList = ({ profiles }: { profiles: ProfileModel[] }) => {
+const ProfileList = async ({
+  filters,
+}: {
+  filters: Record<JobOfferFiltersEnum, string>
+}) => {
   // const shuffledProfiles = shuffleArray(profiles)
+  const profiles = await getPublishedProfilesPayload()
+
+  const filteredProfiles = profiles
+    .filter(filterByPosition(filters.position?.split(',')))
+    .filter(filterBySeniority(filters.seniority?.split(',')))
+    .filter(filterByLocation(filters.location?.split(',')))
+    .filter(filterByTechnology(filters.technology?.split(',')))
+    .filter(filterByAvailability(filters.availability?.split(',')))
 
   return (
     <div className={styles.profileCards}>
       <div className={styles.profileListCont}>
-        {profiles.map((profile) => (
+        {filteredProfiles.map((profile) => (
           <ProfileListItem key={profile.id} data={profile} />
         ))}
       </div>
