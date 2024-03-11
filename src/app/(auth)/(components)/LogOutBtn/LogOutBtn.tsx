988c7c08dc95155339c2bf7481499314ad0c988f
Jakub Wąsowski <jakub@nerdz.pl>
2024-03-11 23:05:52 +0100

    fix: cleanup

--- src/app/(auth)/(components)/LogOutBtn/LogOutBtn.tsx
+++ src/app/(auth)/(components)/LogOutBtn/LogOutBtn.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { Button } from '@/components/Button/Button'
 import { AppRoutes } from '@/utils/routes'
-import { signOut, useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
 import { useState } from 'react'
 
 const LogOutBtn = () => {
-  const { data: session } = useSession()
   const [isCalled, setIsCalled] = useState(false)
   return (
     <div>
