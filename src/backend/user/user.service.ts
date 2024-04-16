a1e7d718bce892a2645d7a927a193dfdb44b43d5
Jakub Wąsowski <jakub@nerdz.pl>
2024-04-17 01:18:52 +0200

    fix: fixing

--- src/backend/user/user.service.ts
+++ src/backend/user/user.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prismaClient'
-import { type Prisma, type Role } from '@prisma/client'
+import { type Role } from '@prisma/client'
 
 export async function getUserById(id: string) {
   const userById = await prisma.user.findFirst({
@@ -46,67 +46,6 @@ export async function findUserById(id: string) {
   })
 
   return foundUser
-}
-
-export async function syncUserWithGithub(credentials: {
-  username: string
-  email: string
-}) {
-  const foundUser = await prisma.user.findFirst({
-    where: {
-      OR: [
-        { githubDetails: { username: credentials.username } },
-        { email: credentials.email },
-      ],
-    },
-    include: {
-      githubDetails: true,
-    },
-  })
-
-  if (foundUser) {
-    const hasEmailChanged = foundUser.email !== credentials.email
-    const hasUsernameChanged =
-      foundUser.githubDetails?.username !== credentials.username
-
-    if (hasEmailChanged || hasUsernameChanged) {
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: foundUser.id,
-        },
-        data: {
-          ...(hasEmailChanged && { email: credentials.email }),
-          ...(hasUsernameChanged && {
-            githubDetails: {
-              update: {
-                username: credentials.username,
-              },
-            },
-          }),
-        },
-      })
-
-      return updatedUser
-    }
-
-    return foundUser
-  }
-
-  return null
-}
-
-export async function updateUserData(
-  email: string,
-  userDataToUpdate: Prisma.ProfileUpdateInput,
-) {
-  const updatedUser = await prisma.user.update({
-    where: {
-      email,
-    },
-    data: userDataToUpdate,
-  })
-
-  return updatedUser
 }
 
 export async function addUserRole(id: string, role: Role) {
@@ -164,16 +103,6 @@ export async function updateAvatar(email: string, avatarUrl: string) {
     where: { email },
     data: { avatarUrl },
   })
-}
-
-export async function getGitHubDetails(id: string) {
-  const userGitHubDetails = await prisma.gitHubDetails.findUnique({
-    where: {
-      userId: id,
-    },
-  })
-
-  return userGitHubDetails
 }
 
 export async function updateUserNerdbordId(userId: string, nerdbordId: string) {
