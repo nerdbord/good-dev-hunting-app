d088e807d53cd6b899503e986556c5b82842b887
Jakub Wąsowski <jakub@nerdz.pl>
2024-04-22 20:11:20 +0200

    chore: add createdAt to user

---
+++ prisma/migrations/20240422181105_add_created_at_to_user_and_profile/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Profile` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
