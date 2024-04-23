65ede1673a48779570298ae4eb6033a19b18a27b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-04-23 16:38:35 +0200

    chore: create profile view model

--- src/app/(profile)/_actions/mutations/createOrUpdateProfileView.ts
+++ src/app/(profile)/_actions/mutations/createOrUpdateProfileView.ts
@@ -1,16 +1,20 @@
 ﻿'use server'
 
+import { createProfileViewModel } from '@/app/(profile)/_models/profile-view.model'
 import {
   createProfileView,
   findProfileViewByViewerIdAndProfileId,
   getProfileById,
   updateProfileView,
 } from '@/backend/profile/profile.service'
+import { type ProfileView } from '@prisma/client'
 
 export const createOrUpdateProfileView = async (
   viewerID: string,
   viewedProfileID: string,
 ) => {
+  let profileView: ProfileView | null = null
+
   const foundProfile = await getProfileById(viewedProfileID)
 
   if (foundProfile?.userId === viewerID) {
@@ -23,10 +27,10 @@ export const createOrUpdateProfileView = async (
   )
 
   if (existingProfileView) {
-    return updateProfileView(existingProfileView.id)
+    profileView = await updateProfileView(existingProfileView.id)
+  } else {
+    profileView = await createProfileView(viewerID, viewedProfileID)
   }
 
-  const newProfileView = await createProfileView(viewerID, viewedProfileID)
-
-  return newProfileView
+  return createProfileViewModel(profileView)
 }
