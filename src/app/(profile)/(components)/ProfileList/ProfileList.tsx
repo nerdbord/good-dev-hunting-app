3fdc7ba0d4f07577e51202bc2dffbbdf0b1e5b38
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-04-23 03:49:25 +0200

    feat: hide talents name when unlogged

--- src/app/(profile)/(components)/ProfileList/ProfileList.tsx
+++ src/app/(profile)/(components)/ProfileList/ProfileList.tsx
@@ -2,10 +2,12 @@
 import { ProfileListItem } from '@/app/(profile)/(components)/ProfileList/ProfileListItem'
 import { useProfiles } from '@/app/(profile)/_providers/Profiles.provider'
 import { AppRoutes } from '@/utils/routes'
+import { useSession } from 'next-auth/react'
 import styles from './ProfileList.module.scss'
 
 const ProfileList = () => {
   const { filteredProfiles: profiles } = useProfiles()
+  const { data: session } = useSession()
 
   if (profiles.length === 0) {
     return (
@@ -20,13 +22,25 @@ const ProfileList = () => {
   return (
     <div className={styles.profileCards}>
       <div className={styles.profileListCont}>
-        {profiles.map((profile) => (
-          <ProfileListItem
-            key={profile.id}
-            data={profile}
-            href={`${AppRoutes.profile}/${profile.githubUsername}`}
-          />
-        ))}
+        {profiles.map((profile) =>
+          session?.user ? (
+            <ProfileListItem
+              key={profile.id}
+              data={profile}
+              href={`${AppRoutes.profile}/${profile.githubUsername}`}
+            />
+          ) : (
+            <ProfileListItem
+              key={profile.id}
+              data={{
+                ...profile,
+                fullName: profile.fullName.slice(0, 1) + '.',
+              }}
+              href={`${AppRoutes.profile}/${profile.githubUsername}`}
+              isHiddenName={true}
+            />
+          ),
+        )}
       </div>
     </div>
   )
