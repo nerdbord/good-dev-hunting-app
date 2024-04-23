c783cfefb4b8855c808bf79355056f086de379d8
The Fox <lisieckikamil93@gmail.com>
2024-04-23 14:11:53 +0200

    fix: add sorted functin in ProfileList to display salary profile first

--- src/app/(profile)/(components)/ProfileList/ProfileList.tsx
+++ src/app/(profile)/(components)/ProfileList/ProfileList.tsx
@@ -7,6 +7,12 @@ import styles from './ProfileList.module.scss'
 const ProfileList = () => {
   const { filteredProfiles: profiles } = useProfiles()
 
+  const sortedProfiles = profiles.sort((a, b) => {
+    if (a.hourlyRateMin && !b.hourlyRateMin) return -1
+    if (!a.hourlyRateMin && b.hourlyRateMin) return 1
+    else return 0
+  })
+
   if (profiles.length === 0) {
     return (
       <div className={styles.profileCards}>
@@ -20,7 +26,7 @@ const ProfileList = () => {
   return (
     <div className={styles.profileCards}>
       <div className={styles.profileListCont}>
-        {profiles.map((profile) => (
+        {sortedProfiles.map((profile) => (
           <ProfileListItem
             key={profile.id}
             data={profile}
