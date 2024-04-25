bfa035b41dd089b52ca4756d6e8244a1c87f9c88
The Fox <lisieckikamil93@gmail.com>
2024-04-25 20:09:51 +0200

    feat: in Loader.tsx implement i18n to test is working depending on /lang

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import { useTranslations } from 'next-intl'
 import styles from './Loader.module.scss'
 
 const Loader = () => {
+  const t = useTranslations('Index')
+
   return (
     <div className={styles.loaderWrapper}>
       <div className={styles.loading}>
-        <p>
+        {/* <p>
           Fetching that data for you <span className={styles.dot1}>.</span>
           <span className={styles.dot2}>.</span>
           <span className={styles.dot3}>.</span>
-        </p>
+        </p> */}
+        {t('title')}
       </div>
     </div>
   )
