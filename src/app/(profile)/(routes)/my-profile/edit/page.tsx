690f44e0f61321998a07cc46b9a9afd7bf092b4a
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-04-26 18:12:30 +0200

    fix: use ProfileProvider on edit page

--- src/app/(profile)/(routes)/my-profile/edit/page.tsx
+++ src/app/(profile)/(routes)/my-profile/edit/page.tsx
@@ -1,11 +1,29 @@
+import { getAuthorizedUser } from '@/app/(auth)/auth.helpers'
 import EditProfileForm from '@/app/(profile)/(routes)/my-profile/(components)/EditProfileForm/EditProfileForm'
+import { findProfileByUserId } from '@/app/(profile)/_actions'
+import { ProfileProvider } from '@/app/(profile)/_providers/Profile.provider'
 import { UploadProvider } from '@/contexts/UploadContext'
+import { AppRoutes } from '@/utils/routes'
+import { redirect } from 'next/navigation'
 
 const EditProfilePage = async () => {
+  const { user, userIsHunter } = await getAuthorizedUser()
+  if (!user || userIsHunter) {
+    redirect(AppRoutes.profilesList)
+  }
+
+  const profile = await findProfileByUserId(user.id)
+
+  if (!profile) {
+    redirect(AppRoutes.createProfile)
+  }
+
   return (
-    <UploadProvider>
-      <EditProfileForm />
-    </UploadProvider>
+    <ProfileProvider profile={profile}>
+      <UploadProvider>
+        <EditProfileForm />
+      </UploadProvider>
+    </ProfileProvider>
   )
 }
 
