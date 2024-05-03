0828c2791bbf7e0a63bb04297b080a9f74556bba
The Fox <lisieckikamil93@gmail.com>
2024-05-03 13:06:10 +0200

    feat: add more translations, in async comp use useTranslation on top level

--- src/components/landing-page/Hero/Hero.tsx
+++ src/components/landing-page/Hero/Hero.tsx
@@ -3,21 +3,22 @@ import MyProfileBtn from '@/app/(profile)/(components)/MyProfileBtn/MyProfileBtn
 import CreateProfileBtn from '@/app/(profile)/(routes)/my-profile/(components)/CreateProfileBtn/CreateProfileBtn'
 import FindTalentsBtn from '@/components/FindTalentsBtn/FindTalentsBtn'
 import { HeroProfilesSection } from '@/components/landing-page/Hero/HeroProfilesSection'
+import { useTranslations } from 'next-intl'
 import styles from './Hero.module.scss'
 
 const Hero = async () => {
+  const t = useTranslations('Index')
+
   const { user, userIsHunter, userHasProfile } = await getAuthorizedUser()
+
   return (
     <section id="hero" className={styles.wrapper}>
       <div className={styles.left}>
         <div className={styles.title}>
-          <span>Let your next project find you</span>
+          <span>{t('hero')}</span>
         </div>
         <div className={styles.subtitle}>
-          <p>
-            Open source, free hub for software developers to be present and
-            ready for next commission work.
-          </p>
+          <p>{t('opensource')}</p>
         </div>
         <div className={styles.buttons}>
           {!userIsHunter && (
