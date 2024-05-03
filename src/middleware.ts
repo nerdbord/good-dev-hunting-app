6db55ea4690c1005eb44ecbc59cddc671b438a93
The Fox <lisieckikamil93@gmail.com>
2024-05-03 15:28:57 +0200

    feat: try to setup new middleware file to handle routing in app

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,54 @@
+// import createMiddleware from 'next-intl/middleware'
+
+// export default createMiddleware({
+//   // A list of all locales that are supported
+//   locales: ['en', 'de', 'pl'],
+
+//   // Used when no locale matches
+//   defaultLocale: 'en',
+// })
+
+// export const config = {
+//   // Match only internationalized pathnames
+//   matcher: ['/', '/(de|en|pl)/:path*'],
+// }
+
 import createMiddleware from 'next-intl/middleware'
 
+// Stwórz middleware używając biblioteki next-intl/middleware
 export default createMiddleware({
-  // A list of all locales that are supported
+  // Lista wszystkich obsługiwanych lokalizacji
   locales: ['en', 'de', 'pl'],
 
-  // Used when no locale matches
+  // Domyślna lokalizacja, używana gdy nie można dopasować żadnej innej
   defaultLocale: 'en',
+
+  // Opcjonalne dodawanie prefiksu lokalizacji tylko gdy jest to niezbędne
+  // Domyślna lokalizacja ('en') nie będzie wymagała prefiksu
+  localePrefix: 'as-needed',
 })
 
+// export const config = {
+//   // Matcher, który identyfikuje ścieżki międzynarodowe
+//   matcher: [
+//     '/', // Domyślna ścieżka
+//     '/(de|en|pl)/:path*', // Ścieżki z prefiksem językowym
+
+//     // Specyficzne ścieżki, które powinny być dostępne z i bez prefiksu lokalizacji
+//     '/profiles',
+//     '/(de|en|pl)/profiles',
+//     '/my-profiles',
+//     '/(de|en|pl)/my-profiles',
+//   ],
+// }
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(de|en|pl)/:path*'],
+  matcher: [
+    '/',
+    '/(de|en|pl)/:path*',
+    '/profiles',
+    '/(de|en|pl)/profiles',
+    '/my-profiles',
+    '/(de|en|pl)/my-profiles',
+  ],
 }
