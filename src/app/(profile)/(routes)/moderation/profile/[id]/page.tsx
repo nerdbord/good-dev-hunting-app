9cda8fb566b5715846df514baf2544a494cf5bc6
Jakub Wąsowski <jakub@nerdz.pl>
2024-05-09 18:23:17 +0200

    fix: roles fix

--- src/app/(profile)/(routes)/moderation/profile/[id]/page.tsx
+++ src/app/(profile)/(routes)/moderation/profile/[id]/page.tsx
@@ -3,11 +3,14 @@ import ModerationActionHeader from '@/app/(profile)/(routes)/moderation/(compone
 import { AppRoutes } from '@/utils/routes'
 import { redirect } from 'next/navigation'
 
+import { findUserById } from '@/app/(auth)/_actions'
+import { findProfileByUserId } from '@/app/(profile)/_actions/queries/findProfileByUserId'
+
 import { getAuthorizedUser } from '@/app/(auth)/auth.helpers'
 import UserProfileDetails from '@/app/(profile)/(components)/UserProfile/UserProfileDetails/UserProfileDetails'
 import UserProfileMain from '@/app/(profile)/(components)/UserProfile/UserProfileMain/UserProfileMain'
-import { findProfileByUserId } from '@/app/(profile)/_actions/queries/findProfileByUserId'
 import { ProfileProvider } from '@/app/(profile)/_providers/Profile.provider'
+
 import styles from './page.module.scss'
 
 export default async function ModerationUserProfile({
@@ -19,13 +22,14 @@ export default async function ModerationUserProfile({
     await getAuthorizedUser()
 
   const profile = await findProfileByUserId(params.id)
+  const profileOwner = await findUserById(params.id)
 
   if (!profile || !authorizedUserIsModerator) redirect(AppRoutes.profilesList)
 
   return (
     <ProfileProvider profile={profile}>
       <div className={styles.wrapper}>
-        <ModerationActionHeader />
+        <ModerationActionHeader profileOwnerRoles={profileOwner.roles} />
         <UserProfileMain profileId={profile.id}>
           <UserProfileHeader profileId={profile.id} withBackButton />
         </UserProfileMain>
