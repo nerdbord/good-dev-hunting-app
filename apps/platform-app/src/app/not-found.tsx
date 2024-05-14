7fcf253e79f38fbaf9579f43427442548dfbe44f
Jakub Wąsowski <jakub@nerdz.pl>
2024-05-15 00:56:08 +0200

    fix: remove old file

--- apps/platform-app/src/app/not-found.tsx
+++ apps/platform-app/src/app/not-found.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 
-export default function NotFound() {
+export const dynamic = 'force-dynamic'
+
+export default async function NotFound() {
   return (
     <div>
       <h2>Not Found</h2>
