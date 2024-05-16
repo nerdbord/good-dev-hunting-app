01d0729630a2d20784c3b96a6492f50141cf44b7
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-05-16 21:26:25 +0200

    dasdasd

--- apps/platform-app/.eslintrc.js
+++ apps/platform-app/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
