03f801a60f38d19a4295244c2422ad893b21583c
fwas <fwas.corp@gmail.com>
2024-05-16 20:01:11 +0200

    fake commit

--- packages/ui-system/.storybook/main.ts
+++ packages/ui-system/.storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 import { dirname, join } from 'path'
 
 /**
+ * 
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
