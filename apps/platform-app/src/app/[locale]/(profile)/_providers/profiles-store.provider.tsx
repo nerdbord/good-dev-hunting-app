da370ced39fbebccea4ab8ed0387c1b1749d49f7
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-05-20 15:14:59 +0200

    feat: instal zustand, create store with profiles

---
+++ apps/platform-app/src/app/[locale]/(profile)/_providers/profiles-store.provider.tsx
@@ -0,0 +1,42 @@
+﻿'use client'
+import { ProfileModel } from '@/app/[locale]/(profile)/_models/profile.model'
+import {
+  ProfilesStore,
+  createProfilesStore,
+} from '@/app/[locale]/(profile)/_stores/profiles-store'
+import { PropsWithChildren, createContext, useContext, useRef } from 'react'
+import { StoreApi, useStore } from 'zustand'
+
+export const ProfilesStoreContext =
+  createContext<StoreApi<ProfilesStore> | null>(null)
+
+export const ProfilesStoreProvider = ({
+  children,
+  initialProfiles,
+}: PropsWithChildren<{ initialProfiles: ProfileModel[] }>) => {
+  const storeRef = useRef<StoreApi<ProfilesStore>>()
+
+  if (!storeRef.current) {
+    storeRef.current = createProfilesStore(initialProfiles)
+  }
+
+  return (
+    <ProfilesStoreContext.Provider value={storeRef.current}>
+      {children}
+    </ProfilesStoreContext.Provider>
+  )
+}
+
+export const useProfilesStore = <T,>(
+  selector: (store: ProfilesStore) => T,
+): T => {
+  const profilesStoreContext = useContext(ProfilesStoreContext)
+
+  if (!profilesStoreContext) {
+    throw new Error(
+      'useProfilesStore must be used within a ProfilesStoreProvider',
+    )
+  }
+
+  return useStore(profilesStoreContext, selector)
+}
