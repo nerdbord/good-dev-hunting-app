51e919407682c0d1e9f0fbe30e1e568c6fea702b
Jakub Wąsowski <jakub@nerdz.pl>
2024-05-21 16:28:09 +0200

    chore: revert deleted buttons

--- packages/ui-system/src/components/index.ts
+++ packages/ui-system/src/components/index.ts
@@ -2,4 +2,6 @@ export { Accordion, AccordionIconType } from './Accordion/Accordion';
 export { Avatar } from './Avatar/Avatar';
 export { AnchorButton } from './Button/AnchorButton';
 export { Button } from './Button/Button';
+// export { GitHubStarsButton } from './Button/GitHubStarsButton';
+// export { MobileGitHubStarsButton } from './Button/MobileGitHubStarsButton';
 export { CheckboxInput } from './Checkbox/Checkbox';
