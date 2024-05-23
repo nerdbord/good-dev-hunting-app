776b558c13acc4628bf72b4068cef723c1c96845
fwas <fwas.corp@gmail.com>
2024-05-23 19:32:51 +0200

    TextArea migrated

---
+++ packages/ui-system/src/stories/TextArea.stories.ts
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { TextArea } from "../index";
+
+const meta = {
+  title: "Components/TextArea/TextArea",
+  component: TextArea,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+    value: "",
+    placeholder: "Enter text here...",
+    onChange: (event) => console.log(event.target.value),
+    name: "textarea",
+    excludeDigits: false,
+    maxLength: 200,
+    height: 100,
+    tooltipText: "Tooltip text",
+    dataTestId: "text-area",
+  },
+};
+
+export const WithImportantIcon: Story = {
+  args: {
+    label: "Label",
+    value: "",
+    placeholder: "Enter text here...",
+    onChange: (event) => console.log(event.target.value),
+    name: "textarea",
+    excludeDigits: false,
+    addImportantIcon: true,
+    maxLength: 200,
+    height: 100,
+    tooltipText: "Tooltip text",
+    dataTestId: "text-area",
+  },
+};
+
+export const ExcludeDigits: Story = {
+  args: {
+    label: "Label",
+    value: "",
+    placeholder: "Enter text here...",
+    onChange: (event) => console.log(event.target.value),
+    name: "textarea",
+    excludeDigits: true,
+    maxLength: 200,
+    height: 100,
+    tooltipText: "Tooltip text",
+    dataTestId: "text-area",
+  },
+};
