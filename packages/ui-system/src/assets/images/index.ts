b1854f12b8d7b2b47d27321f08720176882b1dcb
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-05-28 19:03:11 +0200

    added  images entrypoint

---
+++ packages/ui-system/src/assets/images/index.ts
@@ -0,0 +1,17 @@
+import LogoPNG from './logo.png'
+import ProfilePicture from './ProfilePicture.png'
+import ProfilePicture2 from './ProfilePicture2.png'
+import ProfilePicture3 from './ProfilePicture3.png'
+import Screenshot from './Screenshot.png'
+import UseYourProfile from './UseYourProfile.png'
+import FlagPL from './flagPL.jpg'
+
+export {
+  LogoPNG,
+  ProfilePicture,
+  ProfilePicture2,
+  ProfilePicture3,
+  Screenshot,
+  UseYourProfile,
+  FlagPL,
+}
