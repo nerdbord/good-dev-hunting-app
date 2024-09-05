eb8356e2ead58225c534539f2d4eb6109692a3fd
filwas <fwas.corp@gmail.com>
2024-09-05 19:54:40 +0200

    anchor button now ensures https

--- packages/ui-system/src/components/Button/AnchorButton.tsx
+++ packages/ui-system/src/components/Button/AnchorButton.tsx
@@ -8,11 +8,19 @@ interface AnchorButton {
   icon?: React.ReactElement;
 }
 
+const ensureProtocol = (url: string) => {
+  // Check if the URL starts with "http://" or "https://"
+  if (/^(https?:\/\/)/i.test(url)) {
+    return url; // Return the URL as it is if it has a protocol
+  }
+  return `https://${url}`; // Prepend https:// if no protocol is present
+};
+
 export const AnchorButton = (props: PropsWithChildren<AnchorButton>) => {
   return (
     <Link
       className={styles.socialItem}
-      href={props.href}
+      href={ensureProtocol(props.href)}
       target="_blank"
       rel="noopener noreferrer"
     >
