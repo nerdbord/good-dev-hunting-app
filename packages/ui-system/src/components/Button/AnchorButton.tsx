4cdce0b1ef0aff2def8ce1159312bcbfa11fd9a2
filwas <fwas.corp@gmail.com>
2024-09-06 16:01:09 +0200

    moved helper function into utils folder

--- packages/ui-system/src/components/Button/AnchorButton.tsx
+++ packages/ui-system/src/components/Button/AnchorButton.tsx
@@ -2,19 +2,13 @@ import { type PropsWithChildren } from "react";
 
 import Link from "next/link";
 import styles from "./AnchorButton.module.scss";
+import { ensureProtocol } from "@/utils/routes";
 
 interface AnchorButton {
   href: string;
   icon?: React.ReactElement;
 }
 
-const ensureProtocol = (url: string) => {
-  // Check if the URL starts with "http://" or "https://"
-  if (/^(https?:\/\/)/i.test(url)) {
-    return url; // Return the URL as it is if it has a protocol
-  }
-  return `https://${url}`; // Prepend https:// if no protocol is present
-};
 
 export const AnchorButton = (props: PropsWithChildren<AnchorButton>) => {
   return (
