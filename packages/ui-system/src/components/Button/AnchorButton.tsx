cb9b2058e3b0b1bc26e63609039ccc13b06e49db
filwas <fwas.corp@gmail.com>
2024-09-09 16:20:29 +0200

    move ensureProtocol util from ui-system to platform-app

--- packages/ui-system/src/components/Button/AnchorButton.tsx
+++ packages/ui-system/src/components/Button/AnchorButton.tsx
@@ -2,7 +2,6 @@ import { type PropsWithChildren } from "react";
 
 import Link from "next/link";
 import styles from "./AnchorButton.module.scss";
-import { ensureProtocol } from "@/utils/routes";
 
 interface AnchorButton {
   href: string;
@@ -14,7 +13,7 @@ export const AnchorButton = (props: PropsWithChildren<AnchorButton>) => {
   return (
     <Link
       className={styles.socialItem}
-      href={ensureProtocol(props.href)}
+      href={props.href}
       target="_blank"
       rel="noopener noreferrer"
     >
