1696d7e05bd4bd8df53381e6329f3ef3949b2cd6
filwas <fwas.corp@gmail.com>
2024-09-13 21:13:01 +0200

    profiles work again

--- apps/platform-app/src/app/[locale]/(profile)/(routes)/profiles/page.tsx
+++ apps/platform-app/src/app/[locale]/(profile)/(routes)/profiles/page.tsx
@@ -1,8 +1,6 @@
 import ProfileList from '@/app/[locale]/(profile)/(components)/ProfileList/ProfileList'
 import { type JobOfferFiltersEnum } from '@/app/[locale]/(profile)/profile.types'
 import { Loader } from '@gdh/ui-system'
-import { I18nNamespaces } from '@/i18n'
-import { useTranslations } from 'next-intl'
 import { Suspense } from 'react'
 
 export default async function Profiles({
@@ -10,9 +8,8 @@ export default async function Profiles({
 }: {
   searchParams: Record<JobOfferFiltersEnum, string>
 }) {
-  const t = useTranslations(I18nNamespaces.Index)
   return (
-    <Suspense key={JSON.stringify(searchParams)} fallback={<Loader>{t('title')}</Loader>}>
+    <Suspense key={JSON.stringify(searchParams)} fallback={<Loader></Loader>}>
       <ProfileList />
     </Suspense>
   )
