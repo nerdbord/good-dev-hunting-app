695e9f40e362dce00f8c5f5f7948fd4a8a69d381
transcendence12 <gosiek.krawczuk@gmail.com>
2024-10-25 09:11:34 +0200

    feat: create basic select language element, fix switcher messages bug

--- apps/platform-app/src/app/[locale]/(profile)/(components)/LocaleSwitcher/LocaleSwitcher.tsx
+++ apps/platform-app/src/app/[locale]/(profile)/(components)/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,9 +1,20 @@
+import { I18nNamespaces, locales } from '@/i18n'
+import { useLocale, useTranslations } from 'next-intl'
 import { LocaleSwitcherSelect } from './LocaleSwitcherSelect'
 
 export const LocaleSwitcher = () => {
+  const t = useTranslations(I18nNamespaces.LocaleSwitcher)
+  console.log(t('locale'))
+  const locale = useLocale()
   return (
-    <p>
-      <LocaleSwitcherSelect />
-    </p>
+    <>
+      <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
+        {locales.map((current) => (
+          <option key={current} value={current}>
+            {t('locale', { locale: current })}
+          </option>
+        ))}
+      </LocaleSwitcherSelect>
+    </>
   )
 }
