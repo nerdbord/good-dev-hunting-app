72d111010057d9446adb768a578d512c19595acf
transcendence12 <gosiek.krawczuk@gmail.com>
2024-11-07 14:47:51 +0100

    cleanup unnecessary comments

--- apps/platform-app/src/app/[locale]/(profile)/(components)/LocaleSwitcher/LocaleSwitcher.tsx
+++ apps/platform-app/src/app/[locale]/(profile)/(components)/LocaleSwitcher/LocaleSwitcher.tsx
@@ -2,22 +2,6 @@ import { I18nNamespaces } from '@/i18n'
 import { useLocale, useTranslations } from 'next-intl'
 import { LocaleSwitcherSelect } from './LocaleSwitcherSelect'
 
-// export const LocaleSwitcher = () => {
-//   const t = useTranslations(I18nNamespaces.LocaleSwitcher)
-//   console.log(t('locale'))
-//   const locale = useLocale()
-//   return (
-//     <>
-//       <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
-//         {locales.map((current) => (
-//           <option key={current} value={current}>
-//             {t('locale', { locale: current })}
-//           </option>
-//         ))}
-//       </LocaleSwitcherSelect>
-//     </>
-//   )
-// }
 export const LocaleSwitcher = () => {
   const t = useTranslations(I18nNamespaces.LocaleSwitcher)
   const locale = useLocale()
