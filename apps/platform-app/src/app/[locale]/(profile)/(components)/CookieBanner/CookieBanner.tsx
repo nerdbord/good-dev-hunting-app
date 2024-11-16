4cc904d0d8837e52bc0828fa7089d83dd0f74307
transcendence12 <gosiek.krawczuk@gmail.com>
2024-11-16 21:48:34 +0100

    feat: cookies banner

--- apps/platform-app/src/app/[locale]/(profile)/(components)/CookieBanner/CookieBanner.tsx
+++ apps/platform-app/src/app/[locale]/(profile)/(components)/CookieBanner/CookieBanner.tsx
@@ -1,38 +1,47 @@
 'use client'
+import { getCookie, setCookie } from '@/utils/cookiesHelper'
 import { Button } from '@gdh/ui-system'
+import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 import styles from './CookieBanner.module.scss'
 
 export const CookieBanner: React.FC = () => {
-  const [isBannerVisible, setIsBannerVisible] = useState<boolean>(false)
+  const [cookieConsent, setCookieConsent] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    try {
-      const hasAcceptedCookies = localStorage.getItem('cookiesAccepted')
-      if (!hasAcceptedCookies) {
-        setIsBannerVisible(true)
-      }
-    } catch (error) {
-      console.error('Could not access localStorage:', error)
-    }
+    const storedConsent = getCookie('cookie_consent')
+    setCookieConsent(storedConsent)
+    setIsLoading(false)
   }, [])
 
-  const acceptCookies = () => {
-    try {
-      localStorage.setItem('cookiesAccepted', 'true')
-      setIsBannerVisible(false)
-    } catch (error) {
-      console.error('Could not set cookiesAccepted in localStorage:', error)
+  useEffect(() => {
+    if (cookieConsent !== null) {
+      setCookie('cookie_consent', cookieConsent, 365)
+
+      if (typeof window !== 'undefined' && window.gtag) {
+        window.gtag('consent', 'update', {
+          analytics_storage: cookieConsent === 'granted' ? 'granted' : 'denied',
+        })
+      }
     }
+  }, [cookieConsent])
+
+  // Do not render the banner if loading or consent is already given
+  if (isLoading || cookieConsent !== null) {
+    return null
   }
 
-  const declineCookies = () => {
-    setIsBannerVisible(false)
-  }
+  const handleAcceptCookies = () => setCookieConsent('granted')
+  const handleDeclineCookies = () => setCookieConsent('denied')
 
-  if (!isBannerVisible) return null
+  const bannerClass = classNames(styles.bannerWrapper, {
+    [styles.visible]: cookieConsent === null,
+    [styles.hidden]: cookieConsent !== null,
+  })
+
   return (
-    <div className={styles.bannerWrapper}>
+    <div className={bannerClass}>
       <div className={styles.cookieHeader}>🍪 Our website uses cookies</div>
       <div className={styles.cookieInfoText}>
         Our website use cookies. By continuing, we assume your permission to
@@ -40,10 +49,10 @@ export const CookieBanner: React.FC = () => {
         <span className={styles.cookiePrivacyPolicy}>Privacy Policy</span>.
       </div>
       <div className={styles.btnWrapper}>
-        <Button variant={'primary'} onClick={acceptCookies}>
+        <Button variant={'primary'} onClick={handleAcceptCookies}>
           Accept all
         </Button>
-        <Button variant={'secondary'} onClick={declineCookies}>
+        <Button variant={'secondary'} onClick={handleDeclineCookies}>
           Decline
         </Button>
       </div>
