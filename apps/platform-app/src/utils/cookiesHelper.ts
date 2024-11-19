d339909ca037273601323aa291e05c95f2b6acb8
transcendence12 <gosiek.krawczuk@gmail.com>
2024-11-19 13:17:13 +0100

    feat: adding cookies banner

--- apps/platform-app/src/utils/cookiesHelper.ts
+++ apps/platform-app/src/utils/cookiesHelper.ts
@@ -1,10 +1,26 @@
-export function setCookie(name: string, value: string, days: number) {
-  const expires = new Date()
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000)
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function setServerCookie(
+  name: string,
+  value: string,
+  days: number
+): Promise<void> {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+
+  (await cookies()).set(name, value, {
+    expires,
+    path: "/",
+    secure: true,
+    sameSite: "lax",
+  });
 }
 
-export function getCookie(name: string): string | null {
-  const matches = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))
-  return matches ? decodeURIComponent(matches[2]) : null
+export async function getServerCookie(name: string): Promise<string | null> {
+  const cookieStore = cookies();
+  const cookie = (await cookieStore).get(name);
+  return cookie?.value || null;
 }
+
