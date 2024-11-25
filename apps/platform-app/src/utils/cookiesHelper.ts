6e58785aa77ae80037cb8581d1a43e30ae5968db
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-11-25 23:42:47 +0100

    chore: format all files

--- apps/platform-app/src/utils/cookiesHelper.ts
+++ apps/platform-app/src/utils/cookiesHelper.ts
@@ -1,26 +1,24 @@
-"use server";
+'use server'
 
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers'
 
 export async function setServerCookie(
   name: string,
   value: string,
-  days: number
+  days: number,
 ): Promise<void> {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-
-  (await cookies()).set(name, value, {
+  const expires = new Date()
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000)
+  ;(await cookies()).set(name, value, {
     expires,
-    path: "/",
+    path: '/',
     secure: true,
-    sameSite: "lax",
-  });
+    sameSite: 'lax',
+  })
 }
 
 export async function getServerCookie(name: string): Promise<string | null> {
-  const cookieStore = cookies();
-  const cookie = (await cookieStore).get(name);
-  return cookie?.value || null;
+  const cookieStore = cookies()
+  const cookie = (await cookieStore).get(name)
+  return cookie?.value || null
 }
-
