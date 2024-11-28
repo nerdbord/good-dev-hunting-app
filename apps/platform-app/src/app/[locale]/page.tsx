2d5213a72062fff18945472ffc1d626ba003ca01
Jakub Wąsowski <jakub@nerdz.pl>
2024-11-28 17:30:03 +0100

    feat: setup job post, view, apply + setup landing

--- apps/platform-app/src/app/[locale]/page.tsx
+++ apps/platform-app/src/app/[locale]/page.tsx
@@ -1,35 +1,24 @@
-import { findAllApprovedProfiles } from '@/app/[locale]/(profile)/_actions'
-import { ProfilesStoreProvider } from '@/app/[locale]/(profile)/_providers/profiles-store.provider'
-import {
-  FAQSection,
-  Hero,
-  HowItWorks,
-  LandingFooter,
-  MeetTeam,
-  TalentSection,
-  UseYourProfile,
-} from '@/components/landing-page'
 import { Container } from '@gdh/ui-system'
+
+import BenefitsSection from '@/components/hunter-landing/BenefitsSection/BenefitsSection'
+import CTASection from '@/components/hunter-landing/CTASection/CTASection'
+import HowItWorksHunter from '@/components/hunter-landing/HowItWorksHunter/HowItWorksHunter'
+import HunterHeader from '@/components/hunter-landing/HunterHeader/HunterHeader'
+import HunterHero from '@/components/hunter-landing/HunterHero/HunterHero'
+import SuccessStories from '@/components/hunter-landing/SuccessStories/SuccessStories'
 import styles from '../page.module.scss'
-import Header from './(profile)/(components)/Header/Header'
 
-export default async function LandingPage() {
-  const fetchedProfiles = await findAllApprovedProfiles()
-
+export default async function HunterLandingPage() {
   return (
-    <ProfilesStoreProvider initialProfiles={fetchedProfiles}>
-      <main className={styles.landing_background}>
-        <Header buttonsVariant={'main'} />
-        <Container>
-          <Hero />
-          <HowItWorks />
-          <UseYourProfile />
-          <MeetTeam />
-          <TalentSection />
-          <FAQSection />
-        </Container>
-        <LandingFooter />
-      </main>
-    </ProfilesStoreProvider>
+    <main className={styles.landing_background}>
+      <Container>
+        <HunterHeader />
+        <HunterHero />
+        <HowItWorksHunter />
+        <BenefitsSection />
+        <SuccessStories />
+        <CTASection />
+      </Container>
+    </main>
   )
 }
