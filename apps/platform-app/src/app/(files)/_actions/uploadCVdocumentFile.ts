40efa7509a5bbfaac42bf3b0bd4f3a50d99d43e6
transcendence12 <gosiek.krawczuk@gmail.com>
2024-12-02 15:59:04 +0100

    fix: fix unnecessary comments

--- apps/platform-app/src/app/(files)/_actions/uploadCVdocumentFile.ts
+++ apps/platform-app/src/app/(files)/_actions/uploadCVdocumentFile.ts
@@ -1,6 +1,5 @@
 'use server'
 
-// import { prisma } from '@/lib/prismaClient'
 import { withSentry } from '@/utils/errHandling'
 import { validateCvFile } from '@/utils/validateCvFileHelper'
 import { put } from '@vercel/blob'
@@ -12,65 +11,50 @@ const nanoid = customAlphabet(
   7,
 ) // 7-character random string
 
-// function mapMimeTypeToCvDocumentType(mimeType: string): 'PDF' | 'DOCX' | null {
-//   switch (mimeType) {
-//     case 'application/pdf':
-//       return 'PDF'
-//     case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-//       return 'DOCX'
-//     default:
-//       return null
-//   }
-// }
-
 function mapMimeTypeToCvDocumentType(mimeType: string): 'PDF' | null {
-  if(mimeType === 'application/pdf'){
+  if (mimeType === 'application/pdf') {
     return 'PDF'
   } else {
     return null
   }
 }
 
+export const uploadCVdocumentFile = withSentry(async (form: FormData) => {
+  const cvFile = form.get('cvFileUpload') as File | null
 
+  if (!cvFile) {
+    throw new Error('No file uploaded')
+  }
 
-export const uploadCVdocumentFile = withSentry(
-  async (form: FormData) => {
-    const cvFile = form.get('cvFileUpload') as File | null
-
-    if (!cvFile) {
-      throw new Error('No file uploaded')
+  try {
+    const validatedCVfile = validateCvFile(cvFile)
+    if (!validatedCVfile) {
+      throw new Error('Invalid file')
     }
 
-    try {
-      const validatedCVfile = validateCvFile(cvFile)
-      if (!validatedCVfile) {
-        throw new Error('Invalid file')
-      }
-
-      const cvFileType = mapMimeTypeToCvDocumentType(cvFile.type)
-      if (!cvFileType) {
-        throw new Error('Unsupported file type')
-      }
-
-      const cvFileName = `${nanoid()}.${cvFile.type.split('/')[1]}`
-
-      const blob = await put(cvFileName, cvFile, {
-        access: 'public',
-        addRandomSuffix: false,
-      })
-
-      revalidatePath('/')
-      console.log("CV NAME: ", cvFile.name)
-      console.log("CV URL : ", blob.url)
-      return { success: true, cvUrl: blob.url, cvFile: cvFile.name }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error uploading CV file:', error.message)
-        return { success: false, error: error.message }
-      }
-
-      console.error('Unexpected error during file upload')
-      return { success: false, error: 'Błąd przesyłania pliku' }
+    const cvFileType = mapMimeTypeToCvDocumentType(cvFile.type)
+    if (!cvFileType) {
+      throw new Error('Unsupported file type')
     }
-  },
-)
+
+    const cvFileName = `${nanoid()}.${cvFile.type.split('/')[1]}`
+
+    const blob = await put(cvFileName, cvFile, {
+      access: 'public',
+      addRandomSuffix: false,
+    })
+
+    revalidatePath('/')
+    console.log('CV NAME: ', cvFile.name)
+    console.log('CV URL : ', blob.url)
+    return { success: true, cvUrl: blob.url, cvFile: cvFile.name }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error uploading CV file:', error.message)
+      return { success: false, error: error.message }
+    }
+
+    console.error('Unexpected error during file upload')
+    return { success: false, error: 'Błąd przesyłania pliku' }
+  }
+})
