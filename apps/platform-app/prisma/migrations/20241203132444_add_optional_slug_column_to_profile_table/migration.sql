616e6bec0cd5616881a317a032ef22a64445d2ae
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-12-03 18:53:44 +0100

    feat: add optional slug migration before making it required

--- apps/platform-app/prisma/migrations/20241203132444_add_optional_slug_column_to_profile_table/migration.sql
+++ apps/platform-app/prisma/migrations/20241203132444_add_optional_slug_column_to_profile_table/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[slug]` on the table `Profile` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `slug` to the `Profile` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Profile" ADD COLUMN     "slug" TEXT NOT NULL;
+ALTER TABLE "Profile" ADD COLUMN     "slug" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Profile_slug_key" ON "Profile"("slug");
