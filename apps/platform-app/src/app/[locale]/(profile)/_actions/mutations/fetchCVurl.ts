a0dc5d22ead984c8a73a755d37a5b74fe20fb161
transcendence12 <gosiek.krawczuk@gmail.com>
2024-12-06 15:02:13 +0100

    fix: persist cv

--- apps/platform-app/src/app/[locale]/(profile)/_actions/mutations/fetchCVurl.ts
+++ apps/platform-app/src/app/[locale]/(profile)/_actions/mutations/fetchCVurl.ts
@@ -1,11 +1,11 @@
 'use server'
 
-import { getProfileById } from '@/backend/profile/profile.service'
+import { authorizeUser } from '@/app/[locale]/(auth)/auth.helpers'
+import { findUserByEmail } from '@/backend/user/user.service'
 import { withSentry } from '@/utils/errHandling'
 
-
-export const fetchCVurl = withSentry(async (id: string) => {
-  
-  const profileId = await getProfileById(id)
-  return profileId.cvUrl || null
+export const fetchCVurl = withSentry(async () => {
+  const { email } = await authorizeUser()
+  const user = await findUserByEmail(email)
+  return user?.profile?.cvUrl || null
 })
