2ec81b0faef11fc5cb5030e2e61e8994439ef902
hansac91 <saczukhanna@gmail.com>
2025-01-12 19:59:12 +0100

    feat: cookies adjustment

--- apps/platform-app/src/app/[locale]/(profile)/(components)/ThemeSwitcher/ThemeWrapper.tsx
+++ apps/platform-app/src/app/[locale]/(profile)/(components)/ThemeSwitcher/ThemeWrapper.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useThemeStore } from '@/hooks/useThemeStore'
+import { getServerCookie, setServerCookie } from '@/utils/cookiesHelper'
 import type { ReactNode } from 'react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './ThemeSwitcher.module.scss'
 
 interface ThemeWrapperProps {
@@ -10,9 +11,22 @@ interface ThemeWrapperProps {
 }
 
 export const ThemeWrapper: React.FC<ThemeWrapperProps> = ({ children }) => {
-  const { isDarkTheme } = useThemeStore()
+  const { isDarkTheme, toggleTheme } = useThemeStore()
 
-  console.log('Current theme:', isDarkTheme ? 'dark' : 'light')
+  useEffect(() => {
+    const fetchTheme = async () => {
+      const savedTheme = await getServerCookie('theme')
+      if (savedTheme) {
+        if (savedTheme === 'light' && isDarkTheme) toggleTheme()
+        if (savedTheme === 'dark' && !isDarkTheme) toggleTheme()
+      }
+    }
+    fetchTheme()
+  }, [])
+
+  useEffect(() => {
+    setServerCookie('theme', isDarkTheme ? 'dark' : 'light', 365)
+  }, [isDarkTheme])
 
   return (
     <div
