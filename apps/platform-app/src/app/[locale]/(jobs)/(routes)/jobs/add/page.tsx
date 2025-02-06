4dcb023c47000c1b5574c9210cfd05e62e4aee81
Wiecek-K <krystiandwiecek@gmail.com>
2025-02-06 05:15:50 +0100

    feat: implemented the JobsHeader

--- apps/platform-app/src/app/[locale]/(jobs)/(routes)/jobs/add/page.tsx
+++ apps/platform-app/src/app/[locale]/(jobs)/(routes)/jobs/add/page.tsx
@@ -1,17 +1,26 @@
 import { I18nNamespaces } from '@/i18n/request'
+import { Container } from '@gdh/ui-system'
 import { getTranslations } from 'next-intl/server'
 import { AddJobTopBar } from '../../../(components)/AddJobTopBar/AddJobTopBar'
 import { JobApplicationChat } from '../../../(components)/JobApplicationChat/JobApplicationChat'
+import { JobsHeader } from '../../../(components)/JobsHeader/JobsHeader'
 import { ChatBotProvider } from './utils/ChatBotContext'
 
 const NewJobPage = async () => {
   const t = await getTranslations(I18nNamespaces.AddJobPage)
 
   return (
-    <ChatBotProvider>
-      <AddJobTopBar header={t('header')} description={t('description')} />
-      <JobApplicationChat />
-    </ChatBotProvider>
+    <>
+      <JobsHeader logoWithTagLine={true} />
+      <ChatBotProvider>
+        <main>
+          <Container>
+            <AddJobTopBar header={t('header')} description={t('description')} />
+            <JobApplicationChat />
+          </Container>
+        </main>
+      </ChatBotProvider>
+    </>
   )
 }
 
