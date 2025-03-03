c15060e492c4d6f88b20aa25a781c7b228ae3e95
AniaPiWo <snowflake@wp.pl>
2025-03-03 18:43:03 +0100

    feat: hunter landing setup

---
+++ apps/platform-app/src/components/hunter-landing/HunterFAQ/HunterFAQ.tsx
@@ -0,0 +1,7 @@
+import styles from './HunterFAQ.module.scss'
+
+type Props = {}
+
+export const HunterFAQ = (props: Props) => {
+  return <div className={styles.wrapper}>HunterFAQ</div>
+}
---
+++ apps/platform-app/src/components/hunter-landing/HunterHowItWorks/HunterHowItWorks.tsx
@@ -0,0 +1,7 @@
+import styles from './HunterHowItWorks.module.scss'
+
+type Props = {}
+
+export const HunterHowItWorks = (props: Props) => {
+  return <div className={styles.wrapper}> HunterHowItWorks</div>
+}
