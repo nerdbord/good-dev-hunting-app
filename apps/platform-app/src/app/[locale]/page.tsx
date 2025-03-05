0cfa08d65be5e59da99faabb9a108332f23e1c5c
AniaPiWo <snowflake@wp.pl>
2025-03-05 16:08:54 +0100

    fix: uncomment main page

--- apps/platform-app/src/app/[locale]/page.tsx
+++ apps/platform-app/src/app/[locale]/page.tsx
@@ -1,6 +1,15 @@
 import { Container } from '@gdh/ui-system'
 
+import { HunterCTA } from '@/components/hunter-landing/HunterCTA/HunterCTA'
 import { HunterFAQ } from '@/components/hunter-landing/HunterFAQ/HunterFAQ'
+import { HunterFooter } from '@/components/hunter-landing/HunterFooter/HunterFooter'
+import { HunterHeader } from '@/components/hunter-landing/HunterHeader/HunterHeader'
+import { HunterHero } from '@/components/hunter-landing/HunterHero/HunterHero'
+import { HunterHowItWorks } from '@/components/hunter-landing/HunterHowItWorks/HunterHowItWorks'
+import { HunterReviews } from '@/components/hunter-landing/HunterReviews/HunterReviews'
+import { HunterRiskReducers } from '@/components/hunter-landing/HunterRiskReducers/HunterRiskReducers'
+import { HunterTeam } from '@/components/hunter-landing/HunterTeam/HunterTeam'
+import { HunterWhyWorth } from '@/components/hunter-landing/HunterWhyWorth/HunterWhyWorth'
 
 import styles from './page.module.scss'
 
@@ -8,8 +17,7 @@ export default async function HunterLandingPage() {
   return (
     <main className={styles.hunter_landing}>
       <Container>
-        <HunterFAQ />
-        {/*         <HunterHeader />
+        <HunterHeader />
         <HunterHero />
         <HunterRiskReducers />
         <HunterHowItWorks />
@@ -18,7 +26,7 @@ export default async function HunterLandingPage() {
         <HunterTeam />
         <HunterCTA />
         <HunterFAQ />
-        <HunterFooter /> */}
+        <HunterFooter />
       </Container>
     </main>
   )
