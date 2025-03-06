44bd45fb4b331097a4ad9ef3fb224426f9f0fb63
AniaPiWo <snowflake@wp.pl>
2025-03-06 17:11:27 +0100

    feat: mobile UI

--- apps/platform-app/src/components/hunter-landing/HunterFooter/HunterFooter.tsx
+++ apps/platform-app/src/components/hunter-landing/HunterFooter/HunterFooter.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { I18nNamespaces } from '@/i18n/request'
-import { Button, Container } from '@gdh/ui-system'
+import { Button } from '@gdh/ui-system'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { LogoLight } from '../LogoLight/LogoLight'
@@ -27,42 +27,38 @@ export const HunterFooter = (props: Props) => {
 
   return (
     <section id="HunterFooter" className={styles.footer}>
-      <Container>
-        <div className={styles.topBar}>
-          <LogoLight />
+      <div className={styles.topBar}>
+        <LogoLight />
 
-          <div className={styles.contact}>
-            <div className={styles.contact} onClick={handleContact}>
-              {t('contact')}
-            </div>
-            <div className={styles.contact} onClick={handleJoinAsItSpecialist}>
-              {t('joinAsItSpecialist')}
-            </div>
-            <Button variant="allpurple" onClick={handleAddJob}>
-              {t('addJob')}
-            </Button>
+        <div className={styles.contact}>
+          <div onClick={handleContact}>{t('contact')}</div>
+          <div onClick={handleJoinAsItSpecialist}>
+            {t('joinAsItSpecialist')}
           </div>
+          <Button variant="allpurple" onClick={handleAddJob}>
+            {t('addJob')}
+          </Button>
         </div>
-        <div className={styles.bottomBar}>
-          <span className={styles.copyright}>
-            Copyright © {year} Nerdbord OU All rights reserved.
-          </span>
-          <div className={styles.terms}>
-            <Link
-              target={'_blank'}
-              href="https://glory-licorice-2e2.notion.site/Privacy-policy-6c075e8ad0de4927addf9592bb29de6e?pvs=4"
-            >
-              {t('privacyPolicy')}{' '}
-            </Link>
-            <Link
-              target={'_blank'}
-              href="https://glory-licorice-2e2.notion.site/Good-Dev-Hunting-User-Terms-and-Conditions-77b1c52963f94edbb898a36e2a2ac512"
-            >
-              {t('termsOfService')}{' '}
-            </Link>
-          </div>
+      </div>
+      <div className={styles.bottomBar}>
+        <span className={styles.copyright}>
+          Copyright © {year} Nerdbord OU All rights reserved.
+        </span>
+        <div className={styles.terms}>
+          <Link
+            target={'_blank'}
+            href="https://glory-licorice-2e2.notion.site/Privacy-policy-6c075e8ad0de4927addf9592bb29de6e?pvs=4"
+          >
+            {t('privacyPolicy')}{' '}
+          </Link>
+          <Link
+            target={'_blank'}
+            href="https://glory-licorice-2e2.notion.site/Good-Dev-Hunting-User-Terms-and-Conditions-77b1c52963f94edbb898a36e2a2ac512"
+          >
+            {t('termsOfService')}{' '}
+          </Link>
         </div>
-      </Container>
+      </div>
     </section>
   )
 }
