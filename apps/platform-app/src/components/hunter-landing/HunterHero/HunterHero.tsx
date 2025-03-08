c4804454fa1e2119ba0ea96ae6bb0ceb1037cdb4
transcendence12 <gosiek.krawczuk@gmail.com>
2025-03-08 08:46:26 +0100

    add tech icons

--- apps/platform-app/src/components/hunter-landing/HunterHero/HunterHero.tsx
+++ apps/platform-app/src/components/hunter-landing/HunterHero/HunterHero.tsx
@@ -1,7 +1,7 @@
 import { I18nNamespaces } from '@/i18n/request'
 import { Container } from '@gdh/ui-system'
 import { getTranslations } from 'next-intl/server'
-// import Image from 'next/image'
+import Image from 'next/image'
 import styles from './HunterHero.module.scss'
 import { HeroBottom } from './HeroBottom/HeroBottom'
 type Props = {}
@@ -12,32 +12,53 @@ export const HunterHero = async (props: Props) => {
   return (
     <Container>
       <section id="hero" className={styles.wrapper}>
-        {/* <div className={styles.backgroundIcons}>
-        <div className={`${styles.icon} ${styles.reactIcon}`}>
-          <Image
-            src="/react-landing-icon.png"
-            alt="React"
-            width={80}
-            height={80}
-          />
+        <div className={styles.backgroundIcons}>
+          <div className={`${styles.icon} ${styles.reactIcon}`}>
+            <div className={styles.gradientOuter}>
+              <div className={styles.gradientInner}>
+                <div className={styles.backgroundShadow}>
+                  <Image
+                    src="/LandingHunter/react-svg.svg"
+                    alt="React"
+                    width={48}
+                    height={65}
+                    className={styles.techImage}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.icon} ${styles.pythonIcon}`}>
+            <div className={styles.gradientOuter}>
+              <div className={styles.gradientInner}>
+                <div className={styles.backgroundShadow}>
+                  <Image
+                    src="/LandingHunter/python-svg.svg"
+                    alt="Python"
+                    width={48}
+                    height={65}
+                    className={styles.techImage}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.icon} ${styles.javaIcon}`}>
+            <div className={styles.gradientOuter}>
+              <div className={styles.gradientInner}>
+                <div className={styles.backgroundShadow}>
+                  <Image
+                    src="/LandingHunter/java-svg.svg"
+                    alt="Java"
+                    width={48}
+                    height={65}
+                    className={styles.techImage}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={`${styles.icon} ${styles.pythonIcon}`}>
-          <Image
-            src="/python-landing-icon.png"
-            alt="Python"
-            width={80}
-            height={80}
-          />
-        </div>
-        <div className={`${styles.icon} ${styles.javaIcon}`}>
-          <Image
-            src="/java-sm-landing-icon.png"
-            alt="Java"
-            width={80}
-            height={80}
-          />
-        </div>
-      </div> */}
 
         <div className={styles.topSection}>
           <div className={styles.titleSection}>
