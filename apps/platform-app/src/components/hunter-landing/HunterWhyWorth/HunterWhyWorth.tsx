fde57b033ddbaba53aefd87c74f9ef42a6446180
The Fox <lisieckikamil93@gmail.com>
2025-03-12 14:26:34 +0100

    feat(why-worth): add component with specified dimensions

--- apps/platform-app/src/components/hunter-landing/HunterWhyWorth/HunterWhyWorth.tsx
+++ apps/platform-app/src/components/hunter-landing/HunterWhyWorth/HunterWhyWorth.tsx
@@ -3,5 +3,43 @@ import styles from './HunterWhyWorth.module.scss'
 type Props = {}
 
 export const HunterWhyWorth = (props: Props) => {
-  return <div className={styles.wrapper}>HunterWhyWorth</div>
+  return (
+    <div className={styles.fullWidthWrapper}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Dlaczego warto korzystać z DevHunting?</h2>
+
+        <div className={styles.contentContainer}>
+          <div className={styles.textContainer}>
+            <div className={styles.reason}>
+              <h3 className={styles.reasonTitle}>Całkowicie za darmo</h3>
+              <p className={styles.reasonDescription}>
+                Dodaj swoje zlecenie bez opłat i otrzymaj oferty od specjalistów
+                IT.
+              </p>
+            </div>
+
+            <div className={styles.reason}>
+              <h3 className={styles.reasonTitle}>Prosty i szybki proces</h3>
+              <p className={styles.reasonDescription}>
+                Nie trać czasu na poszukiwania - programiści sami zgłoszą się do
+                Ciebie z dostosowanymi propozycjami.
+              </p>
+            </div>
+
+            <div className={styles.reason}>
+              <h3 className={styles.reasonTitle}>
+                Nie musisz znać technologii
+              </h3>
+              <p className={styles.reasonDescription}>
+                Opisz swój pomysł, a AI przekształci go w brief projektowy dla
+                odpowiednich ekspertów.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.imageContainer} />
+        </div>
+      </div>
+    </div>
+  )
 }
