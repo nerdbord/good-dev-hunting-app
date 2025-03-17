49c98fa7a1db8176b7e3938d7e68b26030869b7c
AniaPiWo <snowflake@wp.pl>
2025-03-17 16:38:33 +0100

    feat: loader variant

--- packages/ui-system/src/components/Loader/Loader.tsx
+++ packages/ui-system/src/components/Loader/Loader.tsx
@@ -1,13 +1,46 @@
+"use client";
+
+import { AppRoutes } from "@/utils/routes";
+import classNames from "classnames/bind";
+import { useLocale } from "next-intl";
+import { usePathname } from "next/navigation";
 import styles from "./Loader.module.scss";
 
-export const Loader = ({ children }: { children?: React.ReactNode }) => {
+const cx = classNames.bind(styles);
+
+export enum LoaderVariant {
+  Default = "defaultLoader",
+  Hunter = "hunterLoader",
+}
+
+interface LoaderProps {
+  children?: React.ReactNode;
+  variant?: LoaderVariant;
+}
+
+export const Loader = ({ children, variant }: LoaderProps) => {
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const isHome = pathname === AppRoutes.home || pathname === `/${locale}`;
+  const loaderVariant =
+    variant ?? (isHome ? LoaderVariant.Hunter : LoaderVariant.Default);
+
   return (
-    <div className={styles.loaderWrapper}>
-      <div className={styles.loading}>
+    <div
+      className={cx("loaderWrapper", {
+        hunterLoaderWrapper: loaderVariant === LoaderVariant.Hunter,
+      })}
+    >
+      <div
+        className={cx("loading", {
+          hunterLoading: loaderVariant === LoaderVariant.Hunter,
+        })}
+      >
         <p>
-          {children} <span className={styles.dot1}>.</span>
-          <span className={styles.dot2}>.</span>
-          <span className={styles.dot3}>.</span>
+          {children} <span className={cx("dot1")}>.</span>
+          <span className={cx("dot2")}>.</span>
+          <span className={cx("dot3")}>.</span>
         </p>
       </div>
     </div>
