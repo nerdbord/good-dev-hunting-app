f1fc65de5e9c76f70146903336953e5f4e237559
AniaPiWo <snowflake@wp.pl>
2025-03-25 13:06:37 +0100

    feat: seo - image, metadata

--- apps/platform-app/src/app/layout.tsx
+++ apps/platform-app/src/app/layout.tsx
@@ -1,4 +1,7 @@
+import { I18nNamespaces } from '@/i18n/request'
 import combineClasses from '@/utils/combineClasses'
+import { type Metadata } from 'next'
+import { getLocale, getTranslations } from 'next-intl/server'
 import { IBM_Plex_Sans, Inter } from 'next/font/google'
 import React from 'react'
 import CookieBannerWrapper from './[locale]/(profile)/(components)/CookieBanner/CookieBannerWrapper'
@@ -19,19 +22,59 @@ const inter = Inter({
 
 const commonClasses = combineClasses([ibm.variable, inter.variable])
 
-export const metadata = {
-  title: 'Good Dev Hunting - find the best developers for your team',
-  description:
-    "Catch coding legends! Good Dev Hunting is reverse recruitment platform that allows you to find the best developers for your team. Our site provides access to detailed profiles of developers, allowing for a quick match of their skills to your company's needs.",
+export async function generateMetadata(): Promise<Metadata> {
+  const locale = await getLocale()
+  const baseUrl = process.env.NEXT_PUBLIC_APP_ORIGIN_URL
+
+  const t = await getTranslations(I18nNamespaces.Metadata)
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: t.raw('keywords'),
+    alternates: {
+      canonical: `${baseUrl}/${locale}`,
+      languages: {
+        pl: `${baseUrl}/pl`,
+        en: `${baseUrl}/en`,
+      },
+    },
+    authors: [{ name: 'GoodDevHunting Team' }],
+    applicationName: 'GoodDevHunting',
+    openGraph: {
+      title: t('title'),
+      description: t('description'),
+      url: `${baseUrl}/${locale}`,
+      siteName: 'GoodDevHunting',
+      locale,
+      type: 'website',
+      images: [
+        {
+          url: `${baseUrl}/opengraph-image.png`,
+          width: 1200,
+          height: 630,
+          alt: t('title'),
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('title'),
+      description: t('description'),
+      images: [`${baseUrl}/opengraph-image.png`],
+    },
+  }
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const locale = await getLocale()
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <GoogleAnalytics
         GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''}
       />
