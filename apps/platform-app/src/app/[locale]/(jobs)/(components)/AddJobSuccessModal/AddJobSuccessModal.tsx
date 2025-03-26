6bccaff8d26f91b8a53958c1549bf1bd132751a9
transcendence12 <gosiek.krawczuk@gmail.com>
2025-03-26 18:16:10 +0100

    create verification and success modal

---
+++ apps/platform-app/src/app/[locale]/(jobs)/(components)/AddJobSuccessModal/AddJobSuccessModal.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { I18nNamespaces } from '@/i18n/request'
+import { Button } from '@gdh/ui-system'
+import { useTranslations } from 'next-intl'
+import styles from './AddJobSuccessModal.module.scss'
+
+export const AddJobSuccessModal = ({
+  closeModal,
+}: {
+  closeModal: () => void
+}) => {
+  const t = useTranslations(I18nNamespaces.AddJobSuccessModal)
+  return (
+    <div className={styles.container} data-testid="addJobSuccessModal">
+      <div className={styles.successContainer}>
+        <div className={styles.successIcon}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="33"
+            height="25"
+            viewBox="0 0 33 25"
+            fill="none"
+          >
+            <path
+              d="M1.5 12.5L11 22L31.5 1.5"
+              stroke="white"
+              strokeWidth="3"
+            />
+          </svg>
+          {/* <span>✔️</span> */}
+        </div>
+      </div>
+
+      <h2 className={styles.header}>{t('title')}</h2>
+      <p className={styles.description}>{t('description')}</p>
+
+      <div className={styles.actions}>
+        <Button variant="primary" onClick={() => {}}>
+          {t('addAnotherJobBtn')}
+        </Button>
+      </div>
+    </div>
+  )
+}
---
+++ apps/platform-app/src/app/[locale]/(jobs)/(components)/AddJobVerificationModal/AddJobVerificationModal.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { I18nNamespaces } from '@/i18n/request'
+import { Button } from '@gdh/ui-system'
+import { useTranslations } from 'next-intl'
+import styles from './AddJobVerificationModal.module.scss'
+
+export const AddJobVerificationModal = ({ closeModal }: { closeModal: () => void }) => {
+  const t = useTranslations(I18nNamespaces.AddJobVerificationModal)
+  return (
+    <div className={styles.container} data-testid="addJobVerificationModal">
+      <div className={styles.loadingContainer}>
+        <div className={styles.loadingDots}></div>
+      </div>
+      
+        <h2 className={styles.header}>{t('title')}</h2>
+        <p className={styles.description}>{t('description')}</p>
+      
+      <div className={styles.actions}>
+        <Button variant="primary" onClick={() => {}}>
+          {t('inProgressBtn')}
+        </Button>
+      </div>
+    </div>
+  )
+}
+ 
