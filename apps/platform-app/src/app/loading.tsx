1e9d6ba3b8d592c1eeebb9be16412d1b2b405dad
transcendence12 <gosiek.krawczuk@gmail.com>
2025-03-31 14:35:28 +0200

    comment LoaderSwitcher for now

--- apps/platform-app/src/app/loading.tsx
+++ apps/platform-app/src/app/loading.tsx
@@ -1,9 +1,10 @@
-import { LoaderSwitcher } from '@/components/Loader/LoaderSwitcher'
+// import { LoaderSwitcher } from '@/components/Loader/LoaderSwitcher'
 import { I18nNamespaces } from '@/i18n/request'
 import { useTranslations } from 'next-intl'
 
 export default function Loading() {
   const t = useTranslations(I18nNamespaces.Index)
 
-  return <LoaderSwitcher message={t('loading')} />
+  // return <LoaderSwitcher message={t('loading')} />
+  return <div>Loading...</div>
 }
