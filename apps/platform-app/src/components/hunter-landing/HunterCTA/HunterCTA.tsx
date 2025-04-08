75589e5e6af383662187a047fd06b8c56ef6ffe0
The Fox <lisieckikamil93@gmail.com>
2025-04-08 15:26:51 +0200

    feat: add hunter CTA section with responsive design

--- apps/platform-app/src/components/hunter-landing/HunterCTA/HunterCTA.tsx
+++ apps/platform-app/src/components/hunter-landing/HunterCTA/HunterCTA.tsx
@@ -1,7 +1,27 @@
+import { I18nNamespaces } from '@/i18n/request'
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
 import styles from './HunterCTA.module.scss'
 
 type Props = {}
 
 export const HunterCTA = (props: Props) => {
-  return <div className={styles.wrapper}>HunterCTA</div>
+  const t = useTranslations(I18nNamespaces.HunterCTA)
+
+  return (
+    <div id="CTA" className={styles.wrapper}>
+      <div className={styles.boxLeft} />
+
+      <div className={styles.content}>
+        <div className={styles.ctaContent}>
+          <h2 className={styles.title}>{t('title')}</h2>
+          <Link href="/add-job" className={styles.button}>
+            {t('button')}
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.boxRight} />
+    </div>
+  )
 }
