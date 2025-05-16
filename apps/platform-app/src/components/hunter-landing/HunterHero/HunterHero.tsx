eabc9505b321a96f7bdbccb0a9de026d8465716b
AniaPiWo <snowflake@wp.pl>
2025-05-16 15:22:25 +0200

    fix: hero hotfix

--- apps/platform-app/src/components/hunter-landing/HunterHero/HunterHero.tsx
+++ apps/platform-app/src/components/hunter-landing/HunterHero/HunterHero.tsx
@@ -1,36 +1,26 @@
 import { I18nNamespaces } from '@/i18n/request'
-import { useTranslations } from 'next-intl'
-import { useCallback } from 'react'
-import styles from './HunterCTA.module.scss'
+import { getTranslations } from 'next-intl/server'
+import { HeroBottom } from './HeroBottom/HeroBottom'
+import styles from './HunterHero.module.scss'
 
-export const HunterCTA = () => {
-  const t = useTranslations(I18nNamespaces.HunterCTA)
-
-  const scrollToHero = useCallback(() => {
-    const heroSection = document.getElementById('hero')
-    if (heroSection) {
-      heroSection.scrollIntoView({ behavior: 'smooth' })
-    }
-  }, [])
+export const HunterHero = async () => {
+  const t = await getTranslations(I18nNamespaces.HunterHero)
 
   return (
-    <div id="CTA" className={styles.wrapper}>
-      <div className={styles.boxLeft} />
+    <section id="hero" className={styles.wrapper}>
+      <div className={styles.topSection}>
+        <div className={styles.titleSection}>
+          <p className={styles.specialistsCount}>{t('specialistsCount')}</p>
+          <h1 className={styles.title}>
+            <span className={styles.titleDesktop}>{t('title')}</span>
 
-      <div className={styles.content}>
-        <div className={styles.ctaContent}>
-          <h2 className={styles.title}>{t('title')}</h2>
-          <button
-            type="button"
-            className={styles.button}
-            onClick={scrollToHero}
-          >
-            {t('button')}
-          </button>
+            <span className={styles.titleMobile}>{t('titleMobile')}</span>
+          </h1>
         </div>
+        <p className={styles.subtitle}>{t('subtitle')}</p>
       </div>
 
-      <div className={styles.boxRight} />
-    </div>
+      <HeroBottom />
+    </section>
   )
 }
